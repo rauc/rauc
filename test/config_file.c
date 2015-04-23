af211680144d0af9e2dfe0aa0daec0f1bcf6c1e4
Jan Luebbe <jluebbe@debian.org>
2015-04-23 16:56:20 +0200

    test: switch to gtest tap mode
    Signed-off-by: Jan Luebbe <jluebbe@debian.org>

--- test/config_file.c
+++ test/config_file.c
@@ -1,10 +1,52 @@
 #include <stdio.h>
+#include <locale.h>
+#include <glib.h>
 
 #include "config_file.h"
 
-int main(int argc, char** argv) {
-	load_config(NULL);
+typedef struct {
+  GKeyFile *key_file;
+} ConfigFileFixture;
 
-	return 0;
+static void config_file_fixture_set_up(ConfigFileFixture *fixture,
+                                       gconstpointer user_data)
+{
+  fixture->key_file = g_key_file_new();
 }
 
+static void config_file_fixture_tear_down(ConfigFileFixture *fixture,
+                                          gconstpointer user_data)
+{
+  g_key_file_free(fixture->key_file);
+}
+
+static void config_file_test1(ConfigFileFixture *fixture,
+                              gconstpointer user_data)
+{
+  load_config(NULL);
+  g_assert_null(user_data);
+}
+
+static void config_file_test2(ConfigFileFixture *fixture,
+                              gconstpointer user_data)
+{
+  load_config(NULL);
+  g_assert_null(user_data);
+}
+
+int main(int argc, char *argv[])
+{
+  setlocale(LC_ALL, "");
+
+  g_test_init(&argc, &argv, NULL);
+
+  g_test_add("/config-file/test1", ConfigFileFixture, NULL,
+             config_file_fixture_set_up, config_file_test1,
+             config_file_fixture_tear_down);
+
+  g_test_add("/config-file/test2", ConfigFileFixture, NULL,
+             config_file_fixture_set_up, config_file_test2,
+             config_file_fixture_tear_down);
+
+  return g_test_run ();
+}
