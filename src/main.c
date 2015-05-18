d6a01c52805acbd645a438812290e1df361524ac
Jan Luebbe <jluebbe@debian.org>
2015-05-18 15:14:53 +0200

    main: use a simple GMainLoop instead of GApplication
    GApplication is targeted at desktop applications and is not a good match
    for our use case.
    
    Signed-off-by: Jan Luebbe <jluebbe@debian.org>

--- src/main.c
+++ src/main.c
@@ -9,17 +9,27 @@
 #include <context.h>
 #include <install.h>
 
+GMainLoop *r_loop = NULL;
+int r_exit_status = 0;
+
 typedef struct {
-	GApplicationCommandLine *cmdline;
 	const gchar *bundlename;
 } RaucInstallArgs;
+
+static gboolean r_quit(gpointer data) {
+	if (r_loop)
+		g_main_loop_quit(r_loop);
+	return G_SOURCE_REMOVE;
+}
 
 static gboolean install_cleanup(gpointer data);
 
 static gboolean install_notify(gpointer data) {
 	RaucInstallArgs *args = data;
 
-	g_application_command_line_print(args->cmdline, "foo!\n");
+	g_message("foo!\n");
+
+	(void)args;
 
 	return FALSE;
 }
@@ -27,9 +37,7 @@ static gboolean install_notify(gpointer data) {
 static gpointer install_thread(gpointer data) {
 	RaucInstallArgs *args = data;
 
-	g_application_command_line_print(args->cmdline,
-					 "thread started for %s\n",
-					 args->bundlename);
+	g_message("thread started for %s\n", args->bundlename);
 	if (g_str_has_suffix(args->bundlename, ".raucb")) {
 		do_install_bundle(args->bundlename);
 	} else {
@@ -41,13 +49,13 @@ static gpointer install_thread(gpointer data) {
 	return NULL;
 }
 
-static gboolean install_start(GApplicationCommandLine *cmdline, int argc, char **argv)
+static gboolean install_start(int argc, char **argv)
 {
 	RaucInstallArgs *args = g_new0(RaucInstallArgs, 1);
 	GThread *thread;
 
 	r_context_set_busy(TRUE);
-	g_application_command_line_print(cmdline, "install started\n");
+	g_message("install started\n");
 
 	if (argc < 3) {
 		g_error("a bundle filename name must be provided");
@@ -56,7 +64,6 @@ static gboolean install_start(GApplicationCommandLine *cmdline, int argc, char *
 
 	g_print("input bundle: %s\n", argv[2]);
 
-	args->cmdline = cmdline;
 	args->bundlename = g_strdup(argv[2]);
 
 	thread = g_thread_new("installer", install_thread, args);
@@ -72,22 +79,20 @@ static gboolean install_cleanup(gpointer data)
 {
 	RaucInstallArgs *args = data;
 
-	g_application_command_line_print(args->cmdline, "install done\n");
-	g_application_command_line_set_exit_status(args->cmdline, 0);
+	g_message("install done\n");
+	r_exit_status = 0;
 
 	/* we are done handling this commandline */
-	g_object_unref(args->cmdline);
 	g_free(args);
 
 	r_context_set_busy(FALSE);
+	g_idle_add(r_quit, NULL);
 	return G_SOURCE_REMOVE;
 }
 
-static gboolean bundle_start(GApplicationCommandLine *cmdline, int argc, char **argv)
+static gboolean bundle_start(int argc, char **argv)
 {
-	int exit_status = 0;
-
-	g_application_command_line_print(cmdline, "bundle start\n");
+	g_message("bundle start\n");
 
 	if (r_context()->certpath == NULL ||
 	    r_context()->keypath == NULL) {
@@ -110,31 +115,27 @@ static gboolean bundle_start(GApplicationCommandLine *cmdline, int argc, char **
 
 	if (!update_manifest(argv[2], FALSE)) {
 		g_print("failed to update manifest\n");
-		exit_status = 1;
+		r_exit_status = 1;
 		goto out;
 	}
 
 	if (!create_bundle(argv[3], argv[2])) {
 		g_print("failed to create bundle\n");
-		exit_status = 1;
+		r_exit_status = 1;
 		goto out;
 	}
 
 out:
-	g_application_command_line_set_exit_status(cmdline, exit_status);
-
 	/* we are done handling this commandline */
-	g_object_unref(cmdline);
-
+	g_idle_add(r_quit, NULL);
 	return G_SOURCE_REMOVE;
 }
 
-static gboolean checksum_start(GApplicationCommandLine *cmdline, int argc, char **argv)
+static gboolean checksum_start(int argc, char **argv)
 {
 	gboolean sign = FALSE;
-	int exit_status = 0;
 
-	g_application_command_line_print(cmdline, "checksum start\n");
+	g_message("checksum start\n");
 
 	if (r_context()->certpath != NULL &&
 	    r_context()->keypath != NULL) {
@@ -154,24 +155,20 @@ static gboolean checksum_start(GApplicationCommandLine *cmdline, int argc, char 
 
 	if (!update_manifest(argv[2], sign)) {
 		g_print("failed to update manifest\n");
-		exit_status = 1;
+		r_exit_status = 1;
 	}
 
 out:
-	g_application_command_line_set_exit_status(cmdline, exit_status);
-
 	/* we are done handling this commandline */
-	g_object_unref(cmdline);
-
+	g_idle_add(r_quit, NULL);
 	return G_SOURCE_REMOVE;
 }
 
-static gboolean info_start(GApplicationCommandLine *cmdline, int argc, char **argv)
+static gboolean info_start(int argc, char **argv)
 {
 	gsize size;
-	int exit_status = 0;
 
-	g_application_command_line_print(cmdline, "info start\n");
+	g_message("info start\n");
 
 	if (argc != 3) {
 		g_error("a file name must be provided");
@@ -181,38 +178,33 @@ static gboolean info_start(GApplicationCommandLine *cmdline, int argc, char **ar
 
 	if (!check_bundle(argv[2], &size)) {
 		g_print("signature invalid (squashfs size: %"G_GSIZE_FORMAT")\n", size);
-		exit_status = 1;
+		r_exit_status = 1;
 		goto out;
 	}
 
 	g_print("signature correct (squashfs size: %"G_GSIZE_FORMAT")\n", size);
 
 out:
-	g_application_command_line_set_exit_status(cmdline, exit_status);
-
 	/* we are done handling this commandline */
-	g_object_unref(cmdline);
-
+	g_idle_add(r_quit, NULL);
 	return G_SOURCE_REMOVE;
 }
 
-static gboolean status_start(GApplicationCommandLine *cmdline, int argc, char **argv)
+static gboolean status_start(int argc, char **argv)
 {
-	g_application_command_line_print(cmdline, "status start\n");
+	g_message("status start\n");
 
 	/* we are done handling this commandline */
-	g_object_unref(cmdline);
-
+	g_idle_add(r_quit, NULL);
 	return G_SOURCE_REMOVE;
 }
 
-static gboolean unknown_start(GApplicationCommandLine *cmdline, int argc, char **argv)
+static gboolean unknown_start(int argc, char **argv)
 {
-	g_application_command_line_print(cmdline, "unknown start\n");
+	g_message("unknown start\n");
 
 	/* we are done handling this commandline */
-	g_object_unref(cmdline);
-
+	g_idle_add(r_quit, NULL);
 	return G_SOURCE_REMOVE;
 }
 
@@ -229,15 +221,12 @@ typedef struct {
 	const RaucCommandType type;
 	const gchar* name;
 	const gchar* usage;
-	gboolean (*cmd_handler) (GApplicationCommandLine *cmdline, int argc, char **argv);
+	gboolean (*cmd_handler) (int argc, char **argv);
 	gboolean while_busy;
 } RaucCommand;
 
-static gboolean cmdline_handler(gpointer data)
+static void cmdline_handler(int argc, char **argv)
 {
-	GApplicationCommandLine *cmdline = data;
-	gchar **args = NULL, **argv = NULL;
-	gint argc;
 	gboolean help = FALSE, version = FALSE;
 	gchar *confpath = NULL, *certpath = NULL, *keypath = NULL, *mount = NULL;
 	GOptionContext *context = NULL;
@@ -262,17 +251,6 @@ static gboolean cmdline_handler(gpointer data)
 	};
 	RaucCommand *rcommand = &rcommands[UNKNOWN];
 
-	g_print("handling command line %p\n", cmdline);
-
-	args = g_application_command_line_get_arguments(cmdline, &argc);
-
-	/* GOptionContext will modify argv, keep the original so we can free
-	 * the strings */
-	argv = g_new(gchar *, argc + 1);
-	for (gint i = 0; i <= argc; i++) {
-		argv[i] = args[i];
-	}
-
 	/* show command-specific usage output */
 	context = g_option_context_new(rcommand->usage);
 
@@ -292,10 +270,9 @@ static gboolean cmdline_handler(gpointer data)
 	}
 
 	if (!g_option_context_parse(context, &argc, &argv, &error)) {
-		g_application_command_line_printerr(cmdline, "%s\n",
-						    error->message);
+		g_error("%s\n", error->message);
 		g_error_free(error);
-		g_application_command_line_set_exit_status(cmdline, 1);
+		r_exit_status = 1;
 		goto done;
 	}
 
@@ -319,12 +296,12 @@ static gboolean cmdline_handler(gpointer data)
 	}
 
 	if (version) {
-		g_application_command_line_print(cmdline, PACKAGE_STRING "\n");
+		g_print(PACKAGE_STRING"\n");
 		goto done;
 	} else if (help || rcommand->type == UNKNOWN) {
 		gchar *text;
 		text = g_option_context_get_help(context, FALSE, NULL);
-		g_application_command_line_print(cmdline, "%s", text);
+		g_print("%s", text);
 		g_free(text);
 		goto done;
 	}
@@ -344,134 +321,39 @@ static gboolean cmdline_handler(gpointer data)
 		if (confpath != NULL ||
 		    certpath != NULL ||
 		    keypath != NULL) {
-			g_application_command_line_printerr(cmdline,
-							    "rauc busy, cannot reconfigure");
-			g_application_command_line_set_exit_status(cmdline, 1);
+			g_error("rauc busy, cannot reconfigure");
+			r_exit_status = 1;
 			goto done;
 		}
 	}
 
 	if (r_context_get_busy() && !rcommand->while_busy) {
-		g_application_command_line_printerr(cmdline,
-						    "rauc busy: cannot run %s",
-						    rcommand->name);
-		g_application_command_line_set_exit_status(cmdline, 1);
+		g_error("rauc busy: cannot run %s", rcommand->name);
+		r_exit_status = 1;
 		goto done;
 	}
 
 	/* real commands are handled here */
 	if (rcommand->cmd_handler) {
-		rcommand->cmd_handler(cmdline, argc, argv);
+		rcommand->cmd_handler(argc, argv);
 		goto delegated;
 	}
 
 done:
 	/* we are done handling this commandline */
-	g_object_unref(cmdline);
+	g_idle_add(r_quit, NULL);
 delegated:
-	g_clear_pointer(&argv, g_free);
-	g_clear_pointer(&args, g_strfreev);
 	g_clear_pointer(&context, g_option_context_free);;
-	return G_SOURCE_REMOVE;
 }
 
-static int command_line(GApplication *application, GApplicationCommandLine *cmdline)
-{
-	/* keep the application running until we are done with this commandline */
-	g_application_hold(application);
+int main(int argc, char **argv) {
+	r_loop = g_main_loop_new(NULL, FALSE);
 
-	g_object_set_data_full(G_OBJECT(cmdline),
-			       "application", application,
-			       (GDestroyNotify) g_application_release);
+	cmdline_handler(argc, argv);
 
-	g_object_ref(cmdline);
-	g_idle_add(cmdline_handler, cmdline);
+	g_main_loop_run(r_loop);
 
-	return 0;
-}
+	g_main_loop_unref(r_loop);
 
-/* Prevents from handling arguments locally as currently all arguments
- * are processed by the remote instance */
-static gboolean
-handle_local_cmdline (GApplication   *application,
-                    gchar        ***arguments,
-                    gint           *exit_status)
-{
-#if 0
-	gint i;
-#endif
-	gchar **argv;
-
-	argv = *arguments;
-
-
-	/* If first option does not start wiht '-' it is assumed to be
-	 * a remote command line call */
-	if (argv[1] && !g_str_has_prefix (argv[1], "-")) {
-		//g_print("This is a REMOTE command\n");
-		return FALSE;
-	}
-
-	/* Do local handling here */
-
-#if 0
-	/* Dummy eater */
-	i = 1;
-	while (argv[i]) {
-		g_free (argv[i]);
-		i++;
-	}
-#endif
-
-	*exit_status = 0;
-
-	return FALSE;
-}
-
-typedef GApplication TestApplication;
-typedef GApplicationClass TestApplicationClass;
-
-static GType test_application_get_type (void);
-G_DEFINE_TYPE (TestApplication, test_application, G_TYPE_APPLICATION)
-
-static void
-test_application_finalize (GObject *object) {
-	G_OBJECT_CLASS (test_application_parent_class)->finalize (object);
-}
-
-static void
-test_application_init (TestApplication *app) {
-}
-
-static void
-test_application_class_init (TestApplicationClass *class) {
-	G_OBJECT_CLASS (class)->finalize = test_application_finalize;
-	G_APPLICATION_CLASS (class)->local_command_line = handle_local_cmdline;
-}
-
-static GApplication *
-test_application_new (const gchar       *application_id,
-                      GApplicationFlags  flags) {
-	g_return_val_if_fail (g_application_id_is_valid (application_id), NULL);
-
-	return g_object_new (test_application_get_type (),
-			"application-id", application_id,
-			"flags", flags,
-			NULL);
-}
-
-int
-main (int argc, char **argv) {
-	GApplication *app;
-	int status;
-
-	app = test_application_new ("de.pengutronix.rauc", G_APPLICATION_HANDLES_COMMAND_LINE);
-	//g_application_set_inactivity_timeout (app, 10000);
-	g_signal_connect (app, "command-line", G_CALLBACK (command_line), NULL);
-
-	status = g_application_run (app, argc, argv);
-
-	g_object_unref (app);
-
-	return status;
+	return r_exit_status;
 }
