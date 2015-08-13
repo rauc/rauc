177a8a1e3c336ad8b0e86c0f6a17d7559b18600f
Enrico Jorns <ejo@pengutronix.de>
2015-08-13 10:40:00 +0200

    Add more error testing for signature.c
    Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

--- test/signature.c
+++ test/signature.c
@@ -5,30 +5,86 @@
 #include <context.h>
 #include <utils.h>
 #include "signature.h"
+#include "common.h"
 
 static void signature_sign(void)
 {
 	GBytes *content = read_file("test/openssl-ca/manifest", NULL);
 	GBytes *sig = NULL;
+	GError *error = NULL;
 	g_assert_nonnull(content);
+
+	// Test valid signing
 	sig = cms_sign(content,
 		       r_context()->certpath,
 		       r_context()->keypath,
-		       NULL);
+		       &error);
 	g_assert_nonnull(sig);
-	g_bytes_unref(content);
+	g_assert_null(error);
+
 	g_bytes_unref(sig);
+
+	// Test signing fail with invalid key
+	sig = cms_sign(content,
+		       r_context()->certpath,
+		       "test/random.dat",
+		       &error);
+	g_assert_null(sig);
+	g_assert_nonnull(error);
+
+	g_clear_error(&error);
+
+	// Test signing fail with invalid cert
+	sig = cms_sign(content,
+		       "test/random.dat",
+		       r_context()->keypath,
+		       &error);
+	g_assert_null(sig);
+	g_assert_nonnull(error);
+
+	g_clear_error(&error);
+
+	g_bytes_unref(content);
 }
 
 static void signature_sign_file(void)
 {
 	GBytes *sig = NULL;
+	GError *error = NULL;
+
+	// Test valid file
 	sig = cms_sign_file("test/openssl-ca/manifest",
 			    r_context()->certpath,
 			    r_context()->keypath,
-			    NULL);
+			    &error);
 	g_assert_nonnull(sig);
+	g_assert_null(error);
+
 	g_bytes_unref(sig);
+	g_clear_error(&error);
+
+
+	// Test non-existing file
+	sig = cms_sign_file("path/to/nonexisting/file",
+			    r_context()->certpath,
+			    r_context()->keypath,
+			    &error);
+	g_assert_null(sig);
+	g_assert_nonnull(error);
+
+	g_bytes_unref(sig);
+	g_clear_error(&error);
+
+	// Test invalid certificate
+	sig = cms_sign_file("test/openssl-ca/manifest",
+			    NULL,
+			    r_context()->keypath,
+			    &error);
+	g_assert_null(sig);
+	g_assert_nonnull(error);
+
+	g_bytes_unref(sig);
+	g_clear_error(&error);
 }
 
 static void signature_verify(void)
@@ -44,10 +100,31 @@ static void signature_verify(void)
 
 static void signature_verify_file(void)
 {
+	GError *error = NULL;
 	GBytes *sig = read_file("test/openssl-ca/manifest-r1.sig", NULL);
+	GBytes *isig = read_file("test/random.dat", NULL);
+
 	g_assert_nonnull(sig);
-	g_assert_true(cms_verify_file("test/openssl-ca/manifest", sig, 0, NULL));
+	g_assert_nonnull(isig);
+
+	// Test valid manifest
+	g_assert_true(cms_verify_file("test/openssl-ca/manifest", sig, 0, &error));
+	g_assert_null(error);
+
+	// Test non-existing file
+	g_assert_false(cms_verify_file("path/to/nonexisting/file", sig, 0, &error));
+	g_assert_nonnull(error);
+
+	g_clear_error(&error);
+
+	// Test valid manifest against invalid signature
+	g_assert_false(cms_verify_file("test/openssl-ca/manifest", isig, 0, &error));
+	g_assert_nonnull(error);
+
+	g_clear_error(&error);
+
 	g_bytes_unref(sig);
+	g_bytes_unref(isig);
 }
 
 static void signature_loopback(void)
@@ -75,6 +152,12 @@ int main(int argc, char *argv[])
 	r_context_conf()->certpath = g_strdup("test/openssl-ca/rel/release-1.cert.pem");
 	r_context_conf()->keypath = g_strdup("test/openssl-ca/rel/private/release-1.pem");
 	r_context();
+
+	g_assert(test_prepare_dummy_file("test/", "random.dat",
+				         256 * 1024, "/dev/urandom") == 0);
+
+	g_assert(test_prepare_dummy_file("test/", "empty.dat",
+				         0, "/dev/zero") == 0);
 
 	g_test_init(&argc, &argv, NULL);
 
