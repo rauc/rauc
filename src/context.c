5ab8506455804f4132baecc27b81fe28f6b6bc28
Bastian Stender <bst@pengutronix.de>
2016-03-02 11:12:28 +0100

    progress: add progress helper functions and tests
    Functions may provide progress information by calling r_context_begin_step,
    r_context_end_step and r_context_set_step_percentage. Progress state
    (percentage, description and nesting depth) is sent via DBus on begin and end
    of each step.
    
    A function must provide an identifying name and the number of direct substeps
    contained in the step (between begin and end). Percentages are calculated
    automatically if r_context_set_step_percentage is not used.
    
    r_context_end_step expects function success information. If the step did
    not complete successfully, the original number of nested substeps does
    not need to match the number of completed substeps.
    
    Signed-off-by: Bastian Stender <bst@pengutronix.de>

--- src/context.c
+++ src/context.c
@@ -143,6 +143,192 @@ void r_context_set_busy(gboolean busy) {
 	context->busy = busy;
 }
 
+static void r_context_send_progress(gboolean op_finished, gboolean success) {
+	RaucProgressStep *step;
+	RaucProgressStep *iter_step;
+	gfloat percentage = 0;
+
+	/* last step already notified parent, ignore it */
+	GList *iter = g_list_next(context->progress);
+
+	/* "stack" should never be NULL at this point */
+	g_assert_nonnull(context->progress);
+
+	step = context->progress->data;
+
+	/* no step in list left means operation complete */
+	if (!iter)
+		percentage = step->percent_done;
+
+	/* sum up done percentages of all steps */
+	while (iter) {
+		iter_step = iter->data;
+		percentage = percentage + iter_step->percent_done;
+		iter = g_list_next(iter);
+	}
+
+	g_assert_cmpint(percentage, <=, 100);
+
+	/* call installer callback with percentage and message */
+	if (op_finished) {
+		if (success)
+			step->description = g_strdup_printf("%s done.",
+							    step->description);
+		else
+			step->description = g_strdup_printf("%s failed.",
+							    step->description);
+	}
+
+	/* handle missing callback gracefully */
+	if (context->progress_callback)
+		context->progress_callback(percentage, step->description,
+					   g_list_length(context->progress));
+}
+
+void r_context_begin_step(const gchar *name, const gchar *description,
+	 		  gint substeps) {
+
+	RaucProgressStep *step = g_new0(RaucProgressStep, 1);
+	RaucProgressStep *parent;
+
+	/* set properties */
+	step->name = name;
+	step->description = description;
+	step->substeps_total = substeps;
+	step->substeps_done = 0;
+	step->percent_done = 0;
+	step->last_explicit_percent = 0;
+
+	/* calculate percentage */
+	if (context->progress) {
+		parent = context->progress->data;
+		g_assert_cmpint(parent->substeps_total, >, 0);
+
+		/* nesting check */
+		if (parent->substeps_total == parent->substeps_done)
+			g_error("Step nesting wrong: %s contains %s exceeding step limit (%d/%d)",
+				parent->name, step->name,
+				parent->substeps_done + 1,
+				parent->substeps_total);
+
+		step->percent_total = parent->percent_total
+			/ parent->substeps_total;
+
+		g_assert_cmpint(step->percent_total, <=,
+				parent->percent_total);
+		g_assert_cmpint(step->percent_total, <=, 100);
+	} else {
+		/* root step */
+		step->percent_total = 100;
+	}
+
+	/* add step to "stack" */
+	context->progress = g_list_prepend(context->progress, step);
+
+	r_context_send_progress(FALSE, FALSE);
+}
+
+void r_context_end_step(const gchar *name, gboolean success) {
+	RaucProgressStep *step;
+	GList *step_element;
+	RaucProgressStep *parent;
+
+	/* "stack" should never be NULL at this point */
+	g_assert_nonnull(context->progress);
+
+	/* get element from "stack" */
+	step_element = context->progress;
+	step = step_element->data;
+	g_assert_nonnull(step);
+
+	step->percent_done = step->percent_total;
+
+	/* check number of substeps */
+	if (step->substeps_done > step->substeps_total)
+		g_error("Too many substeps: %s (%d/%d)",
+			step->name, step->substeps_done,
+			step->substeps_total);
+
+	if (success && step->substeps_done < step->substeps_total)
+		g_error("Not enough substeps: %s (%d/%d)",
+			step->name, step->substeps_done,
+			step->substeps_total);
+
+	/* mark substeps/percentage as done/complete in case of an error */
+	if (!success)
+		step->substeps_done = step->substeps_total;
+
+	/* mark substeps/percentage as done/complete in case of an error */
+	if (!success)
+		step->substeps_done = step->substeps_total;
+
+	/* ensure that progress step nesting is done correctly */
+	g_assert_cmpstr(step->name, ==, name);
+
+	/* increment step count and percentage on parent step */
+	if (g_list_next(context->progress)) {
+		parent = g_list_next(context->progress)->data;
+		parent->substeps_done++;
+
+		/* clean up explicit percentage */
+		if (step->last_explicit_percent != 0)
+			r_context_set_step_percentage(step->name, 100);
+		else
+			parent->percent_done = parent->percent_done
+				+ step->percent_done;
+
+		g_assert_cmpint(step->percent_done, <=,
+				parent->percent_done);
+	}
+
+	r_context_send_progress(TRUE, success);
+	context->progress = g_list_remove_link(context->progress,
+					       step_element);
+
+	g_list_free(step_element);
+	g_free(step);
+}
+
+void r_context_set_step_percentage(const gchar *name, gint custom_percent) {
+	RaucProgressStep *step;
+	RaucProgressStep *parent;
+	gint percent_difference;
+
+	g_assert_nonnull(context->progress);
+
+	step = context->progress->data;
+	parent = g_list_next(context->progress)->data;
+
+	/* ensure that progress step nesting is done correctly */
+	g_assert_cmpstr(step->name, ==, name);
+
+	/* substeps and setting explicit percentage does not make sense */
+	g_assert_cmpint(step->substeps_total, ==, 0);
+
+	percent_difference = custom_percent - step->last_explicit_percent;
+
+	step->percent_done = step->percent_total
+		* (percent_difference / 100.0f);
+
+	/* pass to parent */
+	if (parent)
+		parent->percent_done = parent->percent_done
+			+ step->percent_done;
+
+	step->last_explicit_percent = custom_percent;
+
+	/* r_context_step_end sends 100% progress step */
+	if (custom_percent != 100)
+		r_context_send_progress(FALSE, FALSE);
+}
+
+void r_context_register_progress_callback(progress_callback progress_cb) {
+	g_assert_nonnull(progress_cb);
+	g_assert_null(context->progress_callback);
+
+	context->progress_callback = progress_cb;
+}
+
 RaucContext *r_context_conf(void) {
 	if (context == NULL) {
 		network_init();
@@ -150,6 +336,7 @@ RaucContext *r_context_conf(void) {
 
 		context = g_new0(RaucContext, 1);
 		context->configpath = g_strdup("/etc/rauc/system.conf");
+		context->progress = NULL;
 	}
 
 	g_assert_false(context->busy);
