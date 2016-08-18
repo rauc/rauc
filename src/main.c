a997c0252904e1a1c76a03def9303f94c6fc57e6
Enrico Jorns <ejo@pengutronix.de>
2016-08-18 12:37:28 +0200

    main: improve error handling and printout in install_start
    Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

--- src/main.c
+++ src/main.c
@@ -136,7 +136,12 @@ static gboolean install_start(int argc, char **argv)
 	if (ENABLE_SERVICE) {
 		installer = r_installer_proxy_new_for_bus_sync(G_BUS_TYPE_SYSTEM,
 			G_DBUS_PROXY_FLAGS_GET_INVALIDATED_PROPERTIES,
-			"de.pengutronix.rauc", "/", NULL, NULL);
+			"de.pengutronix.rauc", "/", NULL, &error);
+		if (installer == NULL) {
+			g_printerr("Error creating proxy: %s\n", error->message);
+			g_error_free (error);
+			goto out_loop;
+		}
 		if (g_signal_connect(installer, "g-properties-changed",
 				     G_CALLBACK(on_installer_changed), args) <= 0) {
 			g_printerr("failed to connect properties-changed signal\n");
