67c0411aa67ea2deb0e1524e0d98e0cad1f6c49a
Enrico Joerns <ejo@pengutronix.de>
2017-02-05 21:11:35 +0100

    doc: conf.py: Fix remaining lowercase variants of RAUC
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- docs/conf.py
+++ docs/conf.py
@@ -200,7 +200,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'rauc.tex', u'rauc Documentation',
+  ('index', 'rauc.tex', u'RAUC Documentation',
    u'Jan Luebbe, Enrico Joerns, Juergen Borleis', 'manual'),
 ]
 
@@ -230,7 +230,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'rauc', u'rauc Documentation',
+    ('index', 'rauc', u'RAUC Documentation',
      [u'Jan Luebbe, Enrico Joerns, Juergen Borleis'], 1)
 ]
 
@@ -244,8 +244,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'rauc', u'rauc Documentation',
-   u'Jan Luebbe, Enrico Joerns, Juergen Borleis', 'rauc', 'rauc Documentation.',
+  ('index', 'rauc', u'RAUC Documentation',
+   u'Jan Luebbe, Enrico Joerns, Juergen Borleis', 'rauc', 'RAUC Documentation.',
    'Miscellaneous'),
 ]
 
