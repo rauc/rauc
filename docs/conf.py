46d8c65a25d2d462dcaa10ed9885601beafb1482
Enrico Joerns <ejo@pengutronix.de>
2017-08-16 06:52:59 +0200

    docs/conf.py: we do not have a _static path, yet
    In order to prevent sphinx from ending up in a loop when trying to copy
    content from _static dir, also added `exclude_patterns` to contain build
    directory.
    
    Fixes warning:
    
    | WARNING: html_static_path entry u'/home/travis/build/ejoerns/rauc/docs/_static' does not exist
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ release = 'v0.1.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -131,7 +131,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
