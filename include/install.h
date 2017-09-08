2a745634f9925b0a28717fabf8098062b386e0a5
Enrico Joerns <ejo@pengutronix.de>
2017-09-08 11:51:45 +0200

    include/install: add function documentations
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- include/install.h
+++ include/install.h
@@ -31,15 +31,70 @@ typedef struct {
 	gint status_result;
 } RaucInstallArgs;
 
+/**
+ * Determines the states (ACTIVE | INACTIVE | BOOTED) of the slots specified in
+ * system configuration.
+ *
+ * @param error return location for a GError
+ *
+ * @return TRUE if succeeded, FALSE if failed
+ */
 gboolean determine_slot_states(GError **error);
 
+/**
+ * Returns hash table of slot classes for that a potential installation target
+ * slot could be determined.
+ *
+ * @param manifest manifest to use
+ *
+ * @return GHashTable mapping a slotclass (gchar*) to a slot instance
+ * (RaucSlot*)
+ */
 GHashTable* determine_target_install_group(RaucManifest *manifest);
 
+/**
+ * Basic bundle installation procedure.
+ *
+ * @param args RaucInstallArgs instance
+ * @param error return location for a GError
+ *
+ * @return TRUE if installation succeeded, FALSE if any critical error occurred
+ */
 gboolean do_install_bundle(RaucInstallArgs *args, GError **error);
+
+/**
+ * Basic network installation procedure.
+ *
+ * NOTE: The network mode of RAUC is deprecated and will be replaced by some
+ * other mechanism in the near future. Do not rely on it for new designs.
+ *
+ * @param url URL to manifest to install
+ * @param error return location for a GError
+ *
+ * @return TRUE if installation succeeded, FALSE if any critical error occurred
+ */
 gboolean do_install_network(const gchar *url, GError **error);
 
-
+/**
+ * Initialize new RaucInstallArgs structure
+ *
+ * @return returns newly allocated RaucInstallArgs.
+ *         Free with install_args_free.
+ */
 RaucInstallArgs *install_args_new(void);
+
+/**
+ * Free allocated RaucInstallArgs structure
+ *
+ * @param args instance to free
+ */
 void install_args_free(RaucInstallArgs *args);
 
+/**
+ * Start a new installer thread.
+ *
+ * @param args RaucInstallArgs instance
+ *
+ * @return TRUE if starting thread succeeded, otherwise FALSE
+ */
 gboolean install_run(RaucInstallArgs *args);
