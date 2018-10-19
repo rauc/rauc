1d58412c7997eaba6dab3925ec7ac0d722d1160b
Enrico Joerns <ejo@pengutronix.de>
2018-10-19 09:55:08 +0200

    uncrustify.sh: modify to use distro uncrstify
    With debian buster, uncrustify from apt is recent enough to give valid
    results.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- uncrustify.sh
+++ uncrustify.sh
@@ -4,8 +4,4 @@ set -ex
 
 cd `dirname $0`
 
-if [ ! -e .uncrustify/build/uncrustify ]; then
-	./build-uncrustify.sh
-fi
-
-.uncrustify/build/uncrustify -c .uncrustify.cfg -l C --replace src/*.c include/*.h test/*.[ch] contrib/cgi/src/*.[ch]
+uncrustify -c .uncrustify.cfg -l C --replace src/*.c include/*.h test/*.[ch] contrib/cgi/src/*.[ch]
