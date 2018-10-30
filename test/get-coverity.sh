b56150191a473930f50ea298dfc264a01dfc6787
Enrico Joerns <ejo@pengutronix.de>
2018-10-30 09:48:10 +0100

    test/get-coverity: replace wget by curl
    We have curl available in docker image anyway, thus use this instead of
    wget.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- test/get-coverity.sh
+++ test/get-coverity.sh
@@ -33,7 +33,7 @@ if [ ! -d $TOOL_BASE ]; then
   # Download Coverity Scan Analysis Tool
   if [ ! -e $TOOL_ARCHIVE ]; then
 	  echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
-	  wget -nv -O $TOOL_ARCHIVE $TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
+	  curl -s -o $TOOL_ARCHIVE $TOOL_URL -d "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
   fi
 
   # Extract Coverity Scan Analysis Tool
