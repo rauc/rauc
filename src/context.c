f4a9d1fe69c22bd8683482ef756b656b1876e304
Enrico Joerns <ejo@pengutronix.de>
2019-01-29 10:52:18 +0100

    src/context: support specifying system variant via system-info handler
    This is a quite flexible variant of providing a RAUC system 'variant'.
    
    The system-info script or executable can for example call a binary to
    obtain the variant, perform some modifications on it etc.
    It simply needs to echo a string of the from
    
      RAUC_SYSTEM_VARIANT=<variant-name>
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- src/context.c
+++ src/context.c
@@ -248,10 +248,15 @@ static void r_context_configure(void)
 
 		g_hash_table_iter_init(&iter, vars);
 		while (g_hash_table_iter_next(&iter, (gpointer*) &key, (gpointer*) &value)) {
-			if (g_strcmp0(key, "RAUC_SYSTEM_SERIAL") == 0)
+			if (g_strcmp0(key, "RAUC_SYSTEM_SERIAL") == 0) {
 				r_context_conf()->system_serial = g_strdup(value);
-			else
+			} else if (g_strcmp0(key, "RAUC_SYSTEM_VARIANT") == 0) {
+				/* set variant (overrides possible previous value) */
+				g_free(r_context_conf()->config->system_variant);
+				r_context_conf()->config->system_variant = g_strdup(value);
+			} else {
 				g_message("Ignoring unknown variable %s", key);
+			}
 		}
 	}
 
