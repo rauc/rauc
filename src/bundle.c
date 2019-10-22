rauc/rauc

commit ca64249cc3da21df5c9570278757d295d7094fa2

src/bundle: always call check_bundle explicitly
Prior to this change, all functions that required to have a checked and
verified bundle called check_bundle on their own.

This had a couple of disadvantages, for example that parameters as for
enabling or disabling verification had to be passed explicitly to all
functions that call check_bundle(),

Now that we have the RaucBundle structure, this enables us to use
check_bundle() as a kind of open() function for the bundles, meaning
that you now have to call check_bundle() explicitly for gaining a
RaucBundle struct. All functions that formerly used check_bundle() now
only get the RaucBundle struct of the opened bundle passed.

This reduces the argument list for most functions while easing the
addition of further functionality and bundle parameters passed.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/bundle.c
@@ -321,18 +321,13 @@ out:
 	return res;
 }
 
-gboolean resign_bundle(const gchar *inpath, const gchar *outpath, GError **error) {
+gboolean resign_bundle(RaucBundle *bundle, const gchar *outpath, GError **error) {
 	GError *ierror = NULL;
 	gboolean res = FALSE;
-	RaucBundle *bundle;
 
-	res = check_bundle(inpath, &bundle, TRUE, &ierror);
-	if (!res) {
-		g_propagate_error(error, ierror);
-		goto out;
-	}
+	g_return_val_if_fail(bundle != NULL, FALSE);
 
-	res = truncate_bundle(inpath, outpath, bundle->size, &ierror);
+	res = truncate_bundle(bundle->path, outpath, bundle->size, &ierror);
 	if (!res) {
 		g_propagate_error(error, ierror);
 		goto out;
@@ -473,23 +468,15 @@ out:
 	return res;
 }
 
-gboolean extract_bundle(const gchar *bundlename, const gchar *outputdir, gboolean verify, GError **error) {
+gboolean extract_bundle(RaucBundle *bundle, const gchar *outputdir, GError **error) {
 	GError *ierror = NULL;
 	gboolean res = FALSE;
 
-	r_context_begin_step("extract_bundle", "Extracting bundle", 2);
+	g_return_val_if_fail(bundle != NULL, FALSE);
 
-	res = check_bundle(bundlename, NULL, verify, &ierror);
-	if (!res) {
-		if (g_error_matches(ierror, R_BUNDLE_ERROR, R_BUNDLE_ERROR_SIGNATURE)) {
-			g_propagate_prefixed_error(error, ierror, "Invalid Bundle: ");
-		} else {
-			g_propagate_error(error, ierror);
-		}
-		goto out;
-	}
+	r_context_begin_step("extract_bundle", "Extracting bundle", 1);
 
-	res = unsquashfs(bundlename, outputdir, NULL, &ierror);
+	res = unsquashfs(bundle->path, outputdir, NULL, &ierror);
 	if (!res) {
 		g_propagate_error(error, ierror);
 		goto out;
@@ -501,21 +488,13 @@ out:
 	return res;
 }
 
-gboolean extract_file_from_bundle(const gchar *bundlename, const gchar *outputdir, const gchar *file, gboolean verify, GError **error) {
+gboolean extract_file_from_bundle(RaucBundle *bundle, const gchar *outputdir, const gchar *file, GError **error) {
 	GError *ierror = NULL;
 	gboolean res = FALSE;
 
-	res = check_bundle(bundlename, NULL, verify, &ierror);
-	if (!res) {
-		if (g_error_matches(ierror, R_BUNDLE_ERROR, R_BUNDLE_ERROR_SIGNATURE)) {
-			g_propagate_prefixed_error(error, ierror, "Invalid Bundle: ");
-		} else {
-			g_propagate_error(error, ierror);
-		}
-		goto out;
-	}
+	g_return_val_if_fail(bundle != NULL, FALSE);
 
-	res = unsquashfs(bundlename, outputdir, file, &ierror);
+	res = unsquashfs(bundle->path, outputdir, file, &ierror);
 	if (!res) {
 		g_propagate_error(error, ierror);
 		goto out;
@@ -526,18 +505,13 @@ out:
 	return res;
 }
 
-gboolean mount_bundle(const gchar *bundlename, const gchar *mountpoint, gboolean verify, GError **error) {
+gboolean mount_bundle(RaucBundle *bundle, const gchar *mountpoint, GError **error) {
 	GError *ierror = NULL;
-	RaucBundle *bundle = NULL;
 	gboolean res = FALSE;
 
-	res = check_bundle(bundlename, &bundle, verify, &ierror);
-	if (!res) {
-		g_propagate_error(error, ierror);
-		goto out;
-	}
+	g_return_val_if_fail(bundle != NULL, FALSE);
 
-	res = r_mount_loop(bundlename, mountpoint, bundle->size, &ierror);
+	res = r_mount_loop(bundle->path, mountpoint, bundle->size, &ierror);
 	if (!res) {
 		g_propagate_error(error, ierror);
 		goto out;
@@ -548,11 +522,13 @@ out:
 	return res;
 }
 
-gboolean umount_bundle(const gchar *bundlename, GError **error) {
+gboolean umount_bundle(RaucBundle *bundle, GError **error) {
 	GError *ierror = NULL;
 	gboolean res = FALSE;
 
-	res = r_umount(bundlename, &ierror);
+	g_return_val_if_fail(bundle != NULL, FALSE);
+
+	res = r_umount(bundle->path, &ierror);
 	if (!res) {
 		g_propagate_error(error, ierror);
 		goto out;

test/bundle.c
@@ -92,18 +92,25 @@ static void bundle_test_create_extract(BundleFixture *fixture,
 		gconstpointer user_data)
 {
 	gchar *outputdir;
+	RaucBundle *bundle = NULL;
 
 	outputdir = g_build_filename(fixture->tmpdir, "output", NULL);
 	g_assert_nonnull(outputdir);
 
-	g_assert_true(extract_bundle(fixture->bundlename, outputdir, TRUE, NULL));
+	g_assert_true(check_bundle(fixture->bundlename, &bundle, TRUE, NULL));
+	g_assert_nonnull(bundle);
+
+	g_assert_true(extract_bundle(bundle, outputdir, NULL));
 	g_assert_true(verify_manifest(outputdir, NULL, FALSE, NULL));
+
+	free_bundle(bundle);
 }
 
 static void bundle_test_create_mount_extract(BundleFixture *fixture,
 		gconstpointer user_data)
 {
 	gchar *mountpoint;
+	RaucBundle *bundle = NULL;
 
 	/* mount needs to run as root */
 	if (!test_running_as_root())
@@ -113,9 +120,14 @@ static void bundle_test_create_mount_extract(BundleFixture *fixture,
 	g_assert_nonnull(mountpoint);
 	g_assert(g_mkdir(mountpoint, 0777) == 0);
 
-	g_assert_true(mount_bundle(fixture->bundlename, mountpoint, FALSE, NULL));
+	g_assert_true(check_bundle(fixture->bundlename, &bundle, FALSE, NULL));
+	g_assert_nonnull(bundle);
+
+	g_assert_true(mount_bundle(bundle, mountpoint, NULL));
 	g_assert_true(verify_manifest(mountpoint, NULL, FALSE, NULL));
-	g_assert_true(umount_bundle(fixture->bundlename, NULL));
+	g_assert_true(umount_bundle(bundle, NULL));
+
+	free_bundle(bundle);
 }
 
 
@@ -123,6 +135,7 @@ static void bundle_test_extract_manifest(BundleFixture *fixture,
 		gconstpointer user_data)
 {
 	gchar *outputdir, *manifestpath;
+	RaucBundle *bundle = NULL;
 
 	outputdir = g_build_filename(fixture->tmpdir, "output", NULL);
 	g_assert_nonnull(outputdir);
@@ -130,8 +143,13 @@ static void bundle_test_extract_manifest(BundleFixture *fixture,
 	manifestpath = g_build_filename(fixture->tmpdir, "/output/manifest.raucm", NULL);
 	g_assert_nonnull(manifestpath);
 
-	g_assert_true(extract_file_from_bundle(fixture->bundlename, outputdir, "manifest.raucm", TRUE, NULL));
+	g_assert_true(check_bundle(fixture->bundlename, &bundle, TRUE, NULL));
+	g_assert_nonnull(bundle);
+
+	g_assert_true(extract_file_from_bundle(bundle, outputdir, "manifest.raucm", NULL));
 	g_assert_true(g_file_test(manifestpath, G_FILE_TEST_EXISTS));
+
+	free_bundle(bundle);
 }
 
 static void bundle_test_resign(BundleFixture *fixture,
@@ -153,14 +171,20 @@ static void bundle_test_resign(BundleFixture *fixture,
 	/* Verify input bundle with dev keyring */
 	r_context()->config->keyring_path = g_strdup("test/openssl-ca/dev-ca.pem");
 	g_assert_true(check_bundle(fixture->bundlename, &bundle, TRUE, NULL));
+
+	g_clear_pointer(&bundle, free_bundle);
+
 	/* Verify input bundle with rel keyring */
 	r_context()->config->keyring_path = g_strdup("test/openssl-ca/rel-ca.pem");
 	g_assert_false(check_bundle(fixture->bundlename, &bundle, TRUE, NULL));
 
 	r_context()->config->keyring_path = g_strdup("test/openssl-ca/dev-ca.pem");
-	res = resign_bundle(fixture->bundlename, resignbundle, &ierror);
+	g_assert_true(check_bundle(fixture->bundlename, &bundle, TRUE, NULL));
+	res = resign_bundle(bundle, resignbundle, &ierror);
 	g_assert_no_error(ierror);
 	g_assert_true(res);
+
+	g_clear_pointer(&bundle, free_bundle);
 
 	/* Verify resigned bundle with dev keyring.
 	 * Note that this evaluates to true as the dev-ca.pem keyring contains
@@ -169,9 +193,14 @@ static void bundle_test_resign(BundleFixture *fixture,
 	 * bundles. */
 	r_context()->config->keyring_path = g_strdup("test/openssl-ca/dev-ca.pem");
 	g_assert_true(check_bundle(resignbundle, &bundle, TRUE, NULL));
+
+	g_clear_pointer(&bundle, free_bundle);
+
 	/* Verify resigned bundle with rel keyring */
 	r_context()->config->keyring_path = g_strdup("test/openssl-ca/rel-ca.pem");
 	g_assert_true(check_bundle(resignbundle, &bundle, TRUE, NULL));
+
+	g_clear_pointer(&bundle, free_bundle);
 }
 
 int main(int argc, char *argv[])

include/bundle.h
@@ -49,67 +49,77 @@ gboolean check_bundle(const gchar *bundlename, RaucBundle **bundle, gboolean ver
  * This will create a copy of a bundle with a new signature but unmodified
  * content.
  *
- * @param inpath filename of the bundle to resign
+ * Note that check_bundle() must be called prior to this, to obtain a
+ * RaucBundle struct.
+ *
+ * @param bundle RaucBundle struct as returned by check_bundle()
  * @param outpath filename of the resigned output bundle
  * @param error Return location for a GError
  *
  * @return TRUE on success, FALSE if an error occurred
  */
-gboolean resign_bundle(const gchar *inpath, const gchar *outpath, GError **error);
+gboolean resign_bundle(RaucBundle *bundle, const gchar *outpath, GError **error);
 
 /**
  * Extract a bundle.
  *
  * This will extract the entire bundle content into a given directory.
  *
- * @param bundlemane filename of the bundle to extract
+ * @param bundle RaucBundle struct as returned by check_bundle()
  * @param outputdir directory to instract content into
- * @param verify If set to true the bundle signature will also be verified, if
- *               set to FALSE this step will be skipped
  * @param error Return location for a GError
+ *
+ * Note that check_bundle() must be called prior to this, to obtain a
+ * RaucBundle struct.
  *
  * @return TRUE on success, FALSE if an error occurred
  */
-gboolean extract_bundle(const gchar *bundlename, const gchar *outputdir, gboolean verify, GError **error);
+gboolean extract_bundle(RaucBundle *bundle, const gchar *outputdir, GError **error);
 
 /**
  * Extract a single file form a bundle.
  *
  * This will extract a single file into a given directory.
  *
- * @param bundlemane filename of the bundle to extract
+ * Note that check_bundle() must be called prior to this, to obtain a
+ * RaucBundle struct.
+ *
+ * @param bundle RaucBundle struct as returned by check_bundle()
  * @param outputdir directory to instract the file into
  * @param file filename of file to extract from bundle
- * @param verify If set to true the bundle signature will also be verified, if
- *               set to FALSE this step will be skipped
  * @param error Return location for a GError
  *
  * @return TRUE on success, FALSE if an error occurred
  */
-gboolean extract_file_from_bundle(const gchar *bundlename, const gchar *outputdir, const gchar *file, gboolean verify, GError **error);
+gboolean extract_file_from_bundle(RaucBundle *bundle, const gchar *outputdir, const gchar *file, GError **error);
 
 /**
  * Mount a bundle.
  *
- * @param bundlemane filename of the bundle to mount
+ * Note that check_bundle() must be called prior to this, to obtain a
+ * RaucBundle struct.
+ *
+ * @param bundle RaucBundle struct as returned by check_bundle()
  * @param mountpoint path to the desired mount point
- * @param verify If set to true the bundle signature will also be verified, if
- *               set to FALSE this step will be skipped
+ * @param size
  * @param error Return location for a GError
  *
  * @return TRUE on success, FALSE if an error occurred
  */
-gboolean mount_bundle(const gchar *bundlename, const gchar *mountpoint, gboolean verify, GError **error);
+gboolean mount_bundle(RaucBundle *bundle, const gchar *mountpoint, GError **error);
 
 /**
  * Unmount a bundle.
  *
- * @param bundlemane filename of the bundle to unmount
+ * Note that check_bundle() must be called prior to this, to obtain a
+ * RaucBundle struct.
+ *
+ * @param bundle RaucBundle struct as returned by check_bundle()
  * @param error Return location for a GError
  *
  * @return TRUE on success, FALSE if an error occurred
  */
-gboolean umount_bundle(const gchar *bundlename, GError **error);
+gboolean umount_bundle(RaucBundle *bundle, GError **error);
 
 /**
  * Frees the memory allocated by a RaucBundle.

src/install.c
@@ -990,6 +990,7 @@ gboolean do_install_bundle(RaucInstallArgs *args, GError **error) {
 	gboolean res = FALSE;
 	gchar* mountpoint;
 	RaucManifest *manifest = NULL;
+	RaucBundle *bundle = NULL;
 	GHashTable *target_group;
 
 	g_assert_nonnull(bundlefile);
@@ -1014,7 +1015,14 @@ gboolean do_install_bundle(RaucInstallArgs *args, GError **error) {
 	// TODO: mount info in context ?
 	g_message("Mounting bundle '%s' to '%s'", bundlefile, mountpoint);
 	install_args_update(args, "Checking and mounting bundle...");
-	res = mount_bundle(bundlefile, mountpoint, TRUE, &ierror);
+
+	res = check_bundle(bundlefile, &bundle, TRUE, &ierror);
+	if (!res) {
+		g_propagate_error(error, ierror);
+		goto out;
+	}
+
+	res = mount_bundle(bundle, mountpoint, &ierror);
 	if (!res) {
 		g_propagate_prefixed_error(
 				error,
@@ -1078,11 +1086,12 @@ gboolean do_install_bundle(RaucInstallArgs *args, GError **error) {
 	res = TRUE;
 
 umount:
-	umount_bundle(mountpoint, NULL);
+	umount_bundle(bundle, NULL);
 	g_rmdir(mountpoint);
 	g_clear_pointer(&mountpoint, g_free);
 
 out:
+	g_clear_pointer(&bundle, free_bundle);
 	g_clear_pointer(&manifest, free_manifest);
 	r_context_end_step("do_install_bundle", res);
 

src/main.c
@@ -262,6 +262,7 @@ out:
 
 static gboolean resign_start(int argc, char **argv)
 {
+	RaucBundle *bundle = NULL;
 	GError *ierror = NULL;
 	g_debug("resign start");
 
@@ -290,7 +291,14 @@ static gboolean resign_start(int argc, char **argv)
 		goto out;
 	}
 
-	if (!resign_bundle(argv[2], argv[3], &ierror)) {
+	if (!check_bundle(argv[2], &bundle, TRUE, &ierror)) {
+		g_printerr("%s\n", ierror->message);
+		g_clear_error(&ierror);
+		r_exit_status = 1;
+		goto out;
+	}
+
+	if (!resign_bundle(bundle, argv[3], &ierror)) {
 		g_printerr("Failed to resign bundle: %s\n", ierror->message);
 		g_clear_error(&ierror);
 		r_exit_status = 1;
@@ -298,6 +306,7 @@ static gboolean resign_start(int argc, char **argv)
 	}
 
 out:
+	g_clear_pointer(&bundle, free_bundle);
 	return TRUE;
 }
 
@@ -589,6 +598,7 @@ static gboolean info_start(int argc, char **argv)
 	gchar* bundledir = NULL;
 	gchar* manifestpath = NULL;
 	RaucManifest *manifest = NULL;
+	RaucBundle *bundle = NULL;
 	GError *error = NULL;
 	gboolean res = FALSE;
 	gchar* (*formatter)(RaucManifest *manifest) = NULL;
@@ -627,7 +637,14 @@ static gboolean info_start(int argc, char **argv)
 	bundledir = g_build_filename(tmpdir, "bundle-content", NULL);
 	manifestpath = g_build_filename(bundledir, "manifest.raucm", NULL);
 
-	res = extract_file_from_bundle(argv[2], bundledir, "manifest.raucm", !info_noverify, &error);
+	res = check_bundle(argv[2], &bundle, !info_noverify, &error);
+	if (!res) {
+		g_printerr("%s\n", error->message);
+		g_clear_error(&error);
+		goto out;
+	}
+
+	res = extract_file_from_bundle(bundle, bundledir, "manifest.raucm", &error);
 	if (!res) {
 		g_printerr("%s\n", error->message);
 		g_clear_error(&error);
@@ -648,6 +665,7 @@ out:
 	if (tmpdir)
 		rm_tree(tmpdir, NULL);
 
+	g_clear_pointer(&bundle, free_bundle);
 	g_clear_pointer(&tmpdir, g_free);
 	g_clear_pointer(&bundledir, g_free);
 	g_clear_pointer(&manifestpath, g_free);

src/service.c
@@ -98,6 +98,7 @@ static gboolean r_on_handle_info(RInstaller *interface,
 	gchar* bundledir = NULL;
 	gchar* manifestpath = NULL;
 	RaucManifest *manifest = NULL;
+	RaucBundle *bundle = NULL;
 	GError *error = NULL;
 	gboolean res = TRUE;
 
@@ -118,7 +119,14 @@ static gboolean r_on_handle_info(RInstaller *interface,
 	bundledir = g_build_filename(tmpdir, "bundle-content", NULL);
 	manifestpath = g_build_filename(bundledir, "manifest.raucm", NULL);
 
-	res = extract_file_from_bundle(arg_bundle, bundledir, "manifest.raucm", TRUE, &error);
+	res = check_bundle(arg_bundle, &bundle, TRUE, &error);
+	if (!res) {
+		g_warning("%s", error->message);
+		g_clear_error(&error);
+		goto out;
+	}
+
+	res = extract_file_from_bundle(bundle, bundledir, "manifest.raucm", &error);
 	if (!res) {
 		g_warning("%s", error->message);
 		g_clear_error(&error);
@@ -152,6 +160,8 @@ out:
 						      G_IO_ERROR_FAILED_HANDLED,
 						      "rauc info error");
 	}
+
+	g_clear_pointer(&bundle, free_bundle);
 
 	return TRUE;
 }

commit 055da57bc6a995c8aade22037e47a0549f289eec

bundle: remove keyring path before signing with new key
sign_bundle() verifies the signature after signing against the given
keyring. When resigning this keyring is most likely not valid anymore.
So remove it and skip signature verification after signing.

Fixes: ca64249 ("src/bundle: always call check_bundle explicitly")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

src/bundle.c
@@ -468,6 +468,10 @@ gboolean resign_bundle(RaucBundle *bundle, const gchar *outpath, GError **error)
 		goto out;
 	}
 
+	/* invalidate keyring to avoid post-signing verification with inappropriate
+	 * keyring */
+	g_clear_pointer(&r_context()->config->keyring_path, g_free);
+
 	res = sign_bundle(outpath, &ierror);
 	if (!res) {
 		g_propagate_error(error, ierror);