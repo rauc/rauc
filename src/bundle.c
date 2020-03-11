1a7944ef4e4d0ee1b15188274e2a53cd90206aa5
Enrico Joerns <ejo@pengutronix.de>
2020-03-11 14:25:03 +0100

    src/bundle: move X509 store setup to own function
    This is a preparation to extend the functionality of store setup.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- src/bundle.c
+++ src/bundle.c
@@ -696,6 +696,33 @@ static gboolean is_remote_scheme(const gchar *scheme)
 	       (g_strcmp0(scheme, "ftp") == 0);
 }
 
+static X509_STORE* setup_store(GError **error)
+{
+	const gchar *load_capath = r_context()->config->keyring_path;
+	const gchar *load_cadir = r_context()->config->keyring_directory;
+	X509_STORE *store = NULL;
+
+	if (!(store = X509_STORE_new())) {
+		g_set_error_literal(
+				error,
+				R_SIGNATURE_ERROR,
+				R_SIGNATURE_ERROR_X509_NEW,
+				"failed to allocate new X509 store");
+		return NULL;
+	}
+	if (!X509_STORE_load_locations(store, load_capath, load_cadir)) {
+		g_set_error(
+				error,
+				R_SIGNATURE_ERROR,
+				R_SIGNATURE_ERROR_CA_LOAD,
+				"failed to load CA file '%s' and/or directory '%s'", load_capath, load_cadir);
+		X509_STORE_free(store);
+		return NULL;
+	}
+
+	return store;
+}
+
 gboolean check_bundle(const gchar *bundlename, RaucBundle **bundle, gboolean verify, GError **error)
 {
 	GError *ierror = NULL;
@@ -843,26 +870,10 @@ gboolean check_bundle(const gchar *bundlename, RaucBundle **bundle, gboolean ver
 	}
 
 	if (verify) {
-		const gchar *load_capath = r_context()->config->keyring_path;
-		const gchar *load_cadir = r_context()->config->keyring_directory;
 		CMS_ContentInfo *cms = NULL;
-		X509_STORE *store = NULL;
-
-		if (!(store = X509_STORE_new())) {
-			g_set_error_literal(
-					error,
-					R_SIGNATURE_ERROR,
-					R_SIGNATURE_ERROR_X509_NEW,
-					"failed to allocate new X509 store");
-			res = FALSE;
-			goto out;
-		}
-		if (!X509_STORE_load_locations(store, load_capath, load_cadir)) {
-			g_set_error(
-					error,
-					R_SIGNATURE_ERROR,
-					R_SIGNATURE_ERROR_CA_LOAD,
-					"failed to load CA file '%s' and/or directory '%s'", load_capath, load_cadir);
+		X509_STORE *store = setup_store(&ierror);
+		if (!store) {
+			g_propagate_error(error, ierror);
 			res = FALSE;
 			goto out;
 		}
