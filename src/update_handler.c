984e9c12d27fe71526bff6307bff5692d8505c8d
Bastian Krause <bst@pengutronix.de>
2021-02-15 16:09:21 +0100

    src/update_handler: run boot-gpt-switch pre install hook after slot determined/checked
    This makes sure that the pre install hook only runs if the sanity checks
    succeeded. On top of that it allows us to pass the (to be activated)
    partition to the hook via environment variables in a future commit.
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- src/update_handler.c
+++ src/update_handler.c
@@ -1459,16 +1459,6 @@ static gboolean img_to_boot_gpt_switch_handler(RaucImage *image, RaucSlot *dest_
 	GError *ierror = NULL;
 	struct boot_switch_partition dest_partition;
 
-	/* run slot pre install hook if enabled */
-	if (hook_name && image->hooks.pre_install) {
-		res = run_slot_hook(hook_name, R_SLOT_HOOK_PRE_INSTALL, image,
-				dest_slot, &ierror);
-		if (!res) {
-			g_propagate_error(error, ierror);
-			goto out;
-		}
-	}
-
 	res = r_gpt_switch_get_inactive_partition(dest_slot->device,
 			&dest_partition, dest_slot->region_start,
 			dest_slot->region_size, &ierror);
@@ -1491,6 +1481,16 @@ static gboolean img_to_boot_gpt_switch_handler(RaucImage *image, RaucSlot *dest_
 				image->checksum.size, dest_partition.size);
 		res = FALSE;
 		goto out;
+	}
+
+	/* run slot pre install hook if enabled */
+	if (hook_name && image->hooks.pre_install) {
+		res = run_slot_hook(hook_name, R_SLOT_HOOK_PRE_INSTALL, image,
+				dest_slot, &ierror);
+		if (!res) {
+			g_propagate_error(error, ierror);
+			goto out;
+		}
 	}
 
 	g_message("Clearing inactive boot partition %d on %s", inactive_part,
