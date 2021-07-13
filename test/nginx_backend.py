186c0afdb86e72a48ef5320680c39089b9b9afa5
Jan Luebbe <jlu@pengutronix.de>
2021-07-13 16:03:20 +0200

    test/nginx_backend: add endpoint which fails every second time
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- test/nginx_backend.py
+++ test/nginx_backend.py
@@ -10,6 +10,16 @@ async def index(request):
     return web.Response(text="OK")
 
 
+@routes.get("/sporadic.raucb")
+async def sporadic_get(request):
+    request.app["sporadic_counter"] += 1
+    if request.app["sporadic_counter"] % 2 == 0:
+        raise web.HTTPInternalServerError()
+    else:
+        return web.FileResponse(path="test/good-verity-bundle.raucb")
+
+
 app = web.Application()
+app["sporadic_counter"] = -1
 app.add_routes(routes)
 web.run_app(app, path="/tmp/backend.sock")
