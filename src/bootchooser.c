612499444067f4b2ef91a40006756c272ca84eed
Enrico Joerns <ejo@pengutronix.de>
2021-09-22 00:15:07 +0200

    src/bootchooser: custom_get_state: be more concrete about inavlid string
    We can report that we expected the string to be either "good" or "bad"
    without actually producing longer error messages if we skip the
    higher level information about the backend we use.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- src/bootchooser.c
+++ src/bootchooser.c
@@ -1589,7 +1589,7 @@ static gboolean custom_get_state(RaucSlot *slot, gboolean *good, GError **error)
 				error,
 				R_BOOTCHOOSER_ERROR,
 				R_BOOTCHOOSER_ERROR_FAILED,
-				"Invalid string obtained from custom bootloader backend: '%s'", ret_str);
+				"Obtained string does not match \"good\" or \"bad\": '%s'", ret_str);
 		return FALSE;
 	}
 
