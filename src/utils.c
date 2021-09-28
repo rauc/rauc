rauc/rauc

commit 5b9fd09f0d25f9b18301cf918cd30189faafe80b

utils: add helper for automatic cleanup of file descriptors
glib's helper macros for creating new auto-cleanup functions are based
on the notion that any given type has precisely one sensible
destructor. File descriptors are just plain "int", but such an int could
represent other things, so we can't just say

G_DEFINE_AUTO_CLEANUP_FREE_FUNC(int, closep, -1)
...
g_auto(int) fd = -1;

We can get around that by either forgoing the glib helpers entirely, or
create another name for the type int and use that in the g_auto()
declarations. This does the latter, so that it looks more natural when
mixed with other uses of g_auto() or g_autoptr().

The alternative would look something like

void close_preserve_errno(int *fd);
...
auto_close_fd fd = -1;

Signed-off-by: Rasmus Villemoes <rasmus.villemoes@prevas.dk>

src/utils.c
@@ -4,8 +4,19 @@
 #include <glib.h>
 #include <glib/gstdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "utils.h"
+
+void close_preserve_errno(int fd)
+{
+	int err;
+
+	err = errno;
+	(void) close(fd);
+	errno = err;
+}
 
 GBytes *read_file(const gchar *filename, GError **error)
 {

include/utils.h
@@ -2,6 +2,18 @@
 
 #include <glib.h>
 
+/* Use
+ *
+ *   g_auto(filedesc) fd = -1
+ *
+ * to declare a file descriptor that will be automatically closed when
+ * fd goes out of scope. The desctructor is guaranteed to preserve
+ * errno.
+ */
+typedef int filedesc;
+void close_preserve_errno(filedesc fd);
+G_DEFINE_AUTO_CLEANUP_FREE_FUNC(filedesc, close_preserve_errno, -1)
+
 #define R_LOG_DOMAIN_SUBPROCESS "rauc-subprocess"
 static inline void r_debug_subprocess(GPtrArray *args)
 {

commit c48a98b153fb409d70b9e02d930f1cad46e15063

utils: Remove redundant include
Fixes: 5b9fd09f0d25 ("utils: add helper for automatic cleanup of file descriptors")
Signed-off-by: Alexander Dahl <ada@thorsis.com>

src/utils.c
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
 
 #include "utils.h"
 