3dafe13865f46de8fc2a5d90fa35aad2baee60c2
Enrico Joerns <ejo@pengutronix.de>
2022-01-13 11:15:38 +0100

    docs/conf.py: add the RAUC logo to generated HTML
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- docs/conf.py
+++ docs/conf.py
@@ -133,7 +133,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'RAUC_Logo_outline.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
