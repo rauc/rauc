rauc/rauc

commit 88ace59204a258dbeae26ffb48155cdd39aa4f33

src: make r_context_configure() public to allow calling explicitly
The benefit of calling this explicitly is that we can handle context
setup issues explicitly.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/service.c
@@ -531,11 +531,16 @@ static gboolean r_on_signal(gpointer user_data)
 
 gboolean r_service_run(void)
 {
+	GError *ierror = NULL;
 	gboolean service_return = TRUE;
 	GBusType bus_type = (!g_strcmp0(g_getenv("DBUS_STARTER_BUS_TYPE"), "session"))
 	                    ? G_BUS_TYPE_SESSION : G_BUS_TYPE_SYSTEM;
 
-	r_context();
+	if (!r_context_configure(&ierror)) {
+		g_printerr("Failed to initialize context: %s", ierror->message);
+		g_clear_error(&ierror);
+		return FALSE;
+	}
 
 	service_loop = g_main_loop_new(NULL, FALSE);
 	g_unix_signal_add(SIGTERM, r_on_signal, NULL);

include/context.h
@@ -167,6 +167,26 @@ RaucContext *r_context_conf(void);
 const RaucContext *r_context(void);
 
 /**
+ * Sets up global context.
+ *
+ * Removes 'pending' flag from the context object.
+ *
+ * * Loads RAUC configuration file (system.conf)
+ * * Reads basic system information like variant, system info, etc. (if
+ *   configured)
+ * * Reads 'bootname' from kernel commandline
+ * * Overrides config file values by commandline argument values where required.
+ *
+ * Note: Must not be called when context is 'busy'.
+ *
+ * @param[out] error Return location for a GError, or NULL
+ *
+ * @return TRUE if context configuration succeeded, otherwise FALSE
+ */
+gboolean r_context_configure(GError **error)
+G_GNUC_WARN_UNUSED_RESULT;
+
+/**
  * Cleans up provided RContextInstallationInfo.
  *
  * @param[in] info RContextInstallationInfo to free

src/context.c
@@ -206,7 +206,7 @@ static gchar* get_variant_from_file(const gchar* filename, GError **error)
 }
 
 
-static gboolean r_context_configure(GError **error)
+gboolean r_context_configure(GError **error)
 {
 	gboolean res = TRUE;
 	GError *ierror = NULL;

commit e342e1a23a08b48a278da5cb90177940ca20d3d9

src/service: add missing line break in g_printerr() call
g_printerr() does not add a line break itself, thus we should add it in
the message string.

Fixes: 88ace592 ("src: make r_context_configure() public to allow calling explicitly")

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/service.c
@@ -550,7 +550,7 @@ gboolean r_service_run(void)
 	                    ? G_BUS_TYPE_SESSION : G_BUS_TYPE_SYSTEM;
 
 	if (!r_context_configure(&ierror)) {
-		g_printerr("Failed to initialize context: %s", ierror->message);
+		g_printerr("Failed to initialize context: %s\n", ierror->message);
 		g_clear_error(&ierror);
 		return FALSE;
 	}