rauc/rauc

commit c2f1e4137718d533909169bea360396273ba54ea

src/install: determine_boot_states: do not abort on first failed slot
The determine_boot_states() method iterates over (boot) slots to determine
their boot status.
If determining the boot status failed for one slot, the method aborted
instantly and determine_boot_states() returned an error.
With this handling, it was kind of random which boot status was set and
which not, because this actually depended on the order the slot were
tested and which slot in this order returned an error.

As a result, even slots for which the boot state could have been
determined as 'good', remained marked 'bad'.

To fix this behavior, do not abort on any failed slot but just print the
error message and continue.
Just save the information that not all slots could be determined
sucessfully and report an error at the end.

This is even more consistent when having in mind that the error of
determine_boot_states() is not handled at all but only emitted in a
g_debug() message. Thus higher layers are not informed why the
respective slot boot state is bad.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/install.c
@@ -200,19 +200,30 @@ gboolean determine_boot_states(GError **error)
 {
 	GHashTableIter iter;
 	RaucSlot *slot;
+	gboolean had_errors;
 
 	/* get boot state */
 	g_hash_table_iter_init(&iter, r_context()->config->slots);
 	while (g_hash_table_iter_next(&iter, NULL, (gpointer*) &slot)) {
 		GError *ierror = NULL;
 
-		if (slot->bootname && !r_boot_get_state(slot, &slot->boot_good, &ierror)) {
-			g_propagate_error(error, ierror);
-			return FALSE;
+		if (!slot->bootname)
+			continue;
+
+		if (!r_boot_get_state(slot, &slot->boot_good, &ierror)) {
+			g_message("Failed to get boot state of %s: %s", slot->name, ierror->message);
+			had_errors = TRUE;
 		}
 	}
 
-	return TRUE;
+	if (had_errors)
+		g_set_error_literal(
+				error,
+				R_SLOT_ERROR,
+				R_SLOT_ERROR_NO_SLOT_WITH_STATE_BOOTED,
+				"Could not determine all boot states");
+
+	return !had_errors;
 }
 
 /* Returns NULL-teminated intern string array of all classes listed in

commit e3d0a2627cff79c1d22f176b4307865f341f1461

src/install: fix error indicator init in determine_boot_states()
When 'had_errors' is not initialized to FALSE, function always returns
an error and rauc status (or service) prints:

| Failed to determine boot states: Could not determine all boot states

Fixes: c2f1e413 ("src/install: determine_boot_states: do not abort on first failed slot")

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

src/install.c
@@ -223,7 +223,7 @@ gboolean determine_boot_states(GError **error)
 {
 	GHashTableIter iter;
 	RaucSlot *slot;
-	gboolean had_errors;
+	gboolean had_errors = FALSE;
 
 	/* get boot state */
 	g_hash_table_iter_init(&iter, r_context()->config->slots);