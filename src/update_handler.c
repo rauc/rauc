rauc/rauc

commit c0f8ad4ed18f3364395372667e70f1a770c59225

src/update_handler: avoid fallback to img_to_raw_handler for special slot types
Otherwise, the wrong handler might be run if support is disabled or a different
image type is supplied.

Also allow *.img for boot-mbr-switch.

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/update_handler.c
@@ -1551,8 +1551,11 @@ RaucUpdatePair updatepairs[] = {
 	{"*.squashfs", "ubivol", img_to_ubivol_handler},
 #if ENABLE_EMMC_BOOT_SUPPORT == 1
 	{"*.img", "boot-emmc", img_to_boot_emmc_handler},
+	{"*", "boot-emmc", NULL},
 #endif
 	{"*.vfat", "boot-mbr-switch", img_to_boot_mbr_switch_handler},
+	{"*.img", "boot-mbr-switch", img_to_boot_mbr_switch_handler},
+	{"*", "boot-mbr-switch", NULL},
 	{"*.img", "*", img_to_raw_handler}, /* fallback */
 	{0}
 };
@@ -1570,7 +1573,7 @@ img_to_slot_handler get_update_handler(RaucImage *mfimage, RaucSlot *dest_slot, 
 
 	g_message("Checking image type for slot type: %s", dest);
 
-	for (RaucUpdatePair *updatepair = updatepairs; updatepair->handler != NULL; updatepair++) {
+	for (RaucUpdatePair *updatepair = updatepairs; updatepair->src != NULL; updatepair++) {
 		if (g_pattern_match_simple(updatepair->src, src) &&
 		    g_pattern_match_simple(updatepair->dest, dest)) {
 			g_message("Image detected as type: %s", updatepair->src);

commit c1704aa5d3ba08bed3fe0601acd9d49b389de9c1

src/update_handler: unconditionally avoid fallback for boot-emmc
We must prevent using the fallback handler match when slot type is
'boot-emmc' regardless of whether eMMC support is enabled or not.

Thus move the corresponding match out of the #if statement.

Fixes: c0f8ad4e

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

src/update_handler.c
@@ -2497,8 +2497,8 @@ RaucUpdatePair updatepairs[] = {
 	{"*.squashfs-zst", "ubivol", img_to_ubivol_handler},
 #if ENABLE_EMMC_BOOT_SUPPORT == 1
 	{"*.img", "boot-emmc", img_to_boot_emmc_handler},
-	{"*", "boot-emmc", NULL},
 #endif
+	{"*", "boot-emmc", NULL},
 	{"*.vfat", "boot-mbr-switch", img_to_boot_mbr_switch_handler},
 	{"*.img", "boot-mbr-switch", img_to_boot_mbr_switch_handler},
 	{"*", "boot-mbr-switch", NULL},