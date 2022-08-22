rauc/rauc

commit a14509f28187abe75a71ab98087af23074feecfe

src/main: remove redundant "Failed getting primary slot:" error string
The same error string prefix is applied in r_boot_get_primary() already,
thus the resulting error messages all look like:

| Failed getting primary slot: Failed getting primary slot: Unable to obtain primary element

Thus simply remove the second prefix here.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/main.c
@@ -1585,7 +1585,7 @@ static gboolean status_start(int argc, char **argv)
 
 		status_print->primary = r_boot_get_primary(&ierror);
 		if (!status_print->primary) {
-			g_printerr("Failed getting primary slot: %s\n", ierror->message);
+			g_printerr("%s\n", ierror->message);
 			g_clear_error(&ierror);
 		}
 

commit 57e54bff941dd4866066ef222dd74b19558924f0

src/bootchooser: really remove redundant error message
Unlike the first attempt (a14509f2) to remove the following redundant message:

| Failed getting primary slot: Failed getting primary slot: Unable to obtain primary slot

this approach now fixes all occurences and provides a more consistent
prefixing scope;
A function must only prefix an error message if it can add information
an ouside caller cannot not add.

Example:
The call of make_me_a_sandwhich() does not need to return error messages
with a prefix "Cannot make a sandwich: " as the caller knows that this
function could not make a sandwich in case of an error and thus the
caller could add the information itself.
However, the function make_me_a_sandwich() could add an error prefix "No
cheese found: " as this is information is not available in the outside
scope.

Without stricter rules on error prefixing we might come across this
issues (and thus double-prefixed error messages) again and again.

Fixes: a14509f2 ("src/main: remove redundant "Failed getting primary slot:" error string")

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/bootchooser.c
@@ -1709,12 +1709,8 @@ RaucSlot* r_boot_get_primary(GError **error)
 		return NULL;
 	}
 
-	if (!slot) {
-		g_propagate_prefixed_error(
-				error,
-				ierror,
-				"Failed getting primary slot: ");
-	}
+	if (!slot)
+		g_propagate_error(error, ierror);
 
 	return slot;
 }

src/main.c
@@ -1809,7 +1809,7 @@ static gboolean status_start(int argc, char **argv)
 
 		status_print->primary = r_boot_get_primary(&ierror);
 		if (!status_print->primary) {
-			g_printerr("%s\n", ierror->message);
+			g_printerr("Failed getting primary slot: %s\n", ierror->message);
 			g_clear_error(&ierror);
 		}
 