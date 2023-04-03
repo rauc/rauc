rauc/rauc

commit a6c17a4024a61ffa94ec413bae3ab94cba730bcc

src/install: avoid leaking string if multpile mount points match
We might have multiple mount entries matching the same device and thus
the same slot. Calling

    s->ext_mount_point = g_strdup(g_unix_mount_get_mount_path(m));

for additional mount points matching the same slot will leak the string
formerly assigned to ext_mount_point.

To avoid this, we skip further matches if ext_mount_point was already
set.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/install.c
@@ -102,6 +102,12 @@ gboolean determine_slot_states(GError **error)
 		RaucSlot *s = find_config_slot_by_device(r_context()->config,
 				devicepath);
 		if (s) {
+			/* We might have multiple mount entries matching the same device and thus the same slot.
+			 * To avoid leaking the string returned by g_unix_mount_get_mount_path() here, we skip all further matches
+			 */
+			if (s->ext_mount_point) {
+				break;
+			}
 			s->ext_mount_point = g_strdup(g_unix_mount_get_mount_path(m));
 			g_debug("Found external mountpoint for slot %s at %s", s->name, s->ext_mount_point);
 		}

commit 8158a2c911138f36c7bf06fb3c60de2a088fd490

src/install: un-break external mount point detection
The memory leakage 'fix' introduced in a6c17a402 ("src/install: avoid
leaking string if multpile mount points match") erroneously uses 'break'
where it should have used 'continue'.
As a result, external mount point detection aborts way too early in some
cases and further external mount points will not be detected then.

This is also not ideal in terms of safety, since it could lead to
unintentionally overriding a still-mounted partition which RAUC refuses
normally.

This affects all RAUC releases since (including) v1.2.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/install.c
@@ -132,7 +132,7 @@ gboolean determine_slot_states(GError **error)
 			 * To avoid leaking the string returned by g_unix_mount_get_mount_path() here, we skip all further matches
 			 */
 			if (s->ext_mount_point) {
-				break;
+				continue;
 			}
 			s->ext_mount_point = g_strdup(g_unix_mount_get_mount_path(m));
 			g_debug("Found external mountpoint for slot %s at %s", s->name, s->ext_mount_point);