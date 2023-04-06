e99b53433a004913b28fd972e4f36427976c5721
Enrico Joerns <ejo@pengutronix.de>
2023-04-06 13:20:34 +0200

    test/context: add more bootslot tests
    This tests parsing of /proc/cmdline for 'rauc.slot', 'root=' and for no
    information at all.
    
    This is required since we have just replaced all usages for
    /proc/cmdline in rauc.t sharness tests by --override-boot-slot and would
    thus loose coverage for cmdline parsing.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- test/context.c
+++ test/context.c
@@ -6,6 +6,30 @@
 
 #include "common.h"
 
+static void test_bootslot_rauc_slot(void)
+{
+	r_context_conf()->configpath = g_strdup("test/test.conf");
+	r_context_conf()->configmode = R_CONTEXT_CONFIG_MODE_REQUIRED;
+	r_context_conf()->mock.proc_cmdline = "quiet root=/dev/dummy rauc.slot=A rootwait";
+	g_clear_pointer(&r_context_conf()->bootslot, g_free);
+
+	g_assert_cmpstr(r_context()->bootslot, ==, "A");
+
+	r_context_clean();
+}
+
+static void test_bootslot_root(void)
+{
+	r_context_conf()->configpath = g_strdup("test/test.conf");
+	r_context_conf()->configmode = R_CONTEXT_CONFIG_MODE_REQUIRED;
+	r_context_conf()->mock.proc_cmdline = "quiet root=/dev/dummy rootwait";
+	g_clear_pointer(&r_context_conf()->bootslot, g_free);
+
+	g_assert_cmpstr(r_context()->bootslot, ==, "/dev/dummy");
+
+	r_context_clean();
+}
+
 static void test_bootslot_external_boot(void)
 {
 	r_context_conf()->configpath = g_strdup("test/test.conf");
@@ -30,15 +54,34 @@ static void test_bootslot_nfs_boot(void)
 	r_context_clean();
 }
 
+static void test_bootslot_no_bootslot(void)
+{
+	r_context_conf()->configpath = g_strdup("test/test.conf");
+	r_context_conf()->configmode = R_CONTEXT_CONFIG_MODE_REQUIRED;
+	r_context_conf()->mock.proc_cmdline = "quiet";
+	g_clear_pointer(&r_context_conf()->bootslot, g_free);
+
+	g_assert_null(r_context()->bootslot);
+
+	r_context_clean();
+}
+
+
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "C");
 
 	g_test_init(&argc, &argv, NULL);
 
+	g_test_add_func("/context/bootslot/rauc-slot", test_bootslot_rauc_slot);
+
+	g_test_add_func("/context/bootslot/root", test_bootslot_root);
+
 	g_test_add_func("/context/bootslot/external_boot", test_bootslot_external_boot);
 
 	g_test_add_func("/context/bootslot/nfs_boot", test_bootslot_nfs_boot);
+
+	g_test_add_func("/context/bootslot/no-bootslot", test_bootslot_no_bootslot);
 
 	return g_test_run();
 }
