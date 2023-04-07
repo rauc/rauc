249f80f4364638fa4ad0e435600b6150ab8fdff6
Enrico Joerns <ejo@pengutronix.de>
2023-04-07 13:51:20 +0200

    src/context: obtain system's boot ID
    This information will be required to determine in which boot we run,
    i.e. if we just restarted the service or if we booted into a new system.
    
    For now, we just print it out (as debug output).
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- src/context.c
+++ src/context.c
@@ -27,6 +27,20 @@ static gchar *regex_match(const gchar *pattern, const gchar *string)
 	return NULL;
 }
 
+static gchar* get_boot_id(void)
+{
+	gchar *contents = NULL;
+	g_autoptr(GError) ierror = NULL;
+
+	if (!g_file_get_contents("/proc/sys/kernel/random/boot_id", &contents, NULL, &ierror)) {
+		g_warning("Failed to get boot_id: %s", ierror->message);
+		return NULL;
+	}
+
+	/* file contains newline, modify in-place */
+	return g_strchomp(contents);
+}
+
 static gchar* get_cmdline_bootname(void)
 {
 	g_autofree gchar *contents = NULL;
@@ -320,6 +334,11 @@ static gboolean r_context_configure_target(GError **error)
 	if (context->bootslot == NULL) {
 		context->bootslot = get_cmdline_bootname();
 	}
+
+	g_clear_pointer(&context->boot_id, g_free);
+
+	context->boot_id = get_boot_id();
+	g_debug("Obtained system boot ID: '%s'", context->boot_id);
 
 	return TRUE;
 }
@@ -749,6 +768,7 @@ void r_context_clean(void)
 		g_clear_pointer(&context->intermediatepaths, g_strfreev);
 		g_clear_pointer(&context->mountprefix, g_free);
 		g_clear_pointer(&context->bootslot, g_free);
+		g_clear_pointer(&context->boot_id, g_free);
 		g_clear_pointer(&context->system_serial, g_free);
 		g_clear_pointer(&context->system_info, g_hash_table_destroy);
 
