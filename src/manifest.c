rauc/rauc

commit 3a58163767d370ca7e4605663a6dc5fcb0a1ced0

src/manifest: calculate manifest checksum on load
Together with 'verity' (or derived) bundle formats, this will allow to
unambiguously identify a bundle, since the verity root hash is also part of the
manifest and thus indirectly both the payload and the meta data are convered by
the hash.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/manifest.c
@@ -270,12 +270,15 @@ gboolean load_manifest_mem(GBytes *mem, RaucManifest **manifest, GError **error)
 	g_autoptr(GKeyFile) key_file = NULL;
 	const gchar *data;
 	gsize length;
+	g_autofree gchar *manifest_checksum = NULL;
 
 	data = g_bytes_get_data(mem, &length);
 	if (data == NULL) {
 		g_set_error(error, R_MANIFEST_ERROR, R_MANIFEST_ERROR_NO_DATA, "No data available");
 		return FALSE;
 	}
+
+	manifest_checksum = g_compute_checksum_for_data(G_CHECKSUM_SHA256, (guchar*) data, length);
 
 	key_file = g_key_file_new();
 
@@ -289,17 +292,28 @@ gboolean load_manifest_mem(GBytes *mem, RaucManifest **manifest, GError **error)
 		return FALSE;
 	}
 
+	(*manifest)->hash = g_steal_pointer(&manifest_checksum);
+
 	return TRUE;
 }
 
 gboolean load_manifest_file(const gchar *filename, RaucManifest **manifest, GError **error)
 {
 	GError *ierror = NULL;
+	g_autofree gchar *data;
+	gsize length;
 	g_autoptr(GKeyFile) key_file = NULL;
+	g_autofree gchar *manifest_checksum = NULL;
+
+	if (!g_file_get_contents(filename, &data, &length, &ierror)) {
+		g_propagate_error(error, ierror);
+		return FALSE;
+	}
+	manifest_checksum = g_compute_checksum_for_data(G_CHECKSUM_SHA256, (guchar*) data, length);
 
 	key_file = g_key_file_new();
 
-	if (!g_key_file_load_from_file(key_file, filename, G_KEY_FILE_NONE, &ierror)) {
+	if (!g_key_file_load_from_data(key_file, data, length, G_KEY_FILE_NONE, &ierror)) {
 		g_propagate_error(error, ierror);
 		return FALSE;
 	}
@@ -308,6 +322,8 @@ gboolean load_manifest_file(const gchar *filename, RaucManifest **manifest, GErr
 		g_propagate_error(error, ierror);
 		return FALSE;
 	}
+
+	(*manifest)->hash = g_steal_pointer(&manifest_checksum);
 
 	return TRUE;
 }

include/manifest.h
@@ -71,6 +71,8 @@ typedef struct {
 
 	/* internal marker that this was encrypted */
 	gboolean was_encrypted;
+	/* computed manifest hash */
+	gchar *hash;
 } RaucManifest;
 
 /**

commit 6642c5cbf99fd873bd3120fafdf98af6fe9c7bee

src/manifest: fix memory leak of manifest hash
Fixes: 3a58163767d3 ("src/manifest: calculate manifest checksum on load")
Signed-off-by: René Fischer <sk@securitykernel.io>
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/manifest.c
@@ -882,6 +882,7 @@ void free_manifest(RaucManifest *manifest)
 	g_free(manifest->hook_name);
 	g_list_free_full(manifest->images, r_free_image);
 	g_clear_pointer(&manifest->meta, (GDestroyNotify)g_hash_table_destroy);
+	g_free(manifest->hash);
 	g_free(manifest);
 }
 