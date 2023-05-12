rauc/rauc

commit 8a1596523a3d3872a6a5c7b970d134bdf1635571

src/context: store obtained infos from system-info handler in context
Before, all keys but 'RAUC_SYSTEM_SERIAL' and 'RAUC_SYSTEM_VARIANT' were
ignored.

Now you can add arbitrary key/value pairs.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/context.c
@@ -215,7 +215,7 @@ static gchar* get_variant_from_file(const gchar* filename, GError **error)
 	return contents;
 }
 
-static gboolean get_system_info_from_handler(GError **error)
+static GHashTable *get_system_info_from_handler(GError **error)
 {
 	g_autoptr(GHashTable) vars = NULL;
 	GError *ierror = NULL;
@@ -225,29 +225,28 @@ static gboolean get_system_info_from_handler(GError **error)
 
 	if (!g_file_test(context->config->systeminfo_handler, G_FILE_TEST_EXISTS)) {
 		g_set_error(error, G_FILE_ERROR, G_FILE_ERROR_NOENT,  "System info handler script/binary '%s' not found.", context->config->systeminfo_handler);
-		return FALSE;
+		return NULL;
 	}
 
 	g_message("Getting Systeminfo: %s", context->config->systeminfo_handler);
 	if (!launch_and_wait_variables_handler(context->config->systeminfo_handler, &vars, &ierror)) {
 		g_propagate_prefixed_error(error, ierror, "Failed to read system-info variables: ");
-		return FALSE;
+		return NULL;
 	}
 
 	g_hash_table_iter_init(&iter, vars);
 	while (g_hash_table_iter_next(&iter, (gpointer*) &key, (gpointer*) &value)) {
+		/* legacy handling */
 		if (g_strcmp0(key, "RAUC_SYSTEM_SERIAL") == 0) {
 			context->system_serial = g_strdup(value);
 		} else if (g_strcmp0(key, "RAUC_SYSTEM_VARIANT") == 0) {
 			/* set variant (overrides possible previous value) */
 			g_free(context->config->system_variant);
 			context->config->system_variant = g_strdup(value);
-		} else {
-			g_message("Ignoring unknown variable %s", key);
 		}
 	}
 
-	return TRUE;
+	return g_steal_pointer(&vars);
 }
 
 /**
@@ -293,10 +292,14 @@ static gboolean r_context_configure_target(GError **error)
 	}
 
 	if (context->config->systeminfo_handler) {
-		if (!get_system_info_from_handler(&ierror)) {
+		context->system_info = get_system_info_from_handler(&ierror);
+		if (!context->system_info) {
 			g_propagate_error(error, ierror);
 			return FALSE;
 		}
+	} else {
+		/* Ensure the hash table is always created so that we do not need to check this later */
+		context->system_info = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 	}
 
 	if (r_whitespace_removed(context->config->system_variant))

include/context.h
@@ -51,6 +51,7 @@ typedef struct {
 	gchar *bootslot;
 
 	gchar *system_serial;
+	GHashTable *system_info; /* key/values of system information */
 
 	/* optional custom handler extra arguments */
 	gchar *handlerextra;

commit ae4b40e877fe1d0b839b54ae91b1a7f1d6d43042

src/context: fix leak of system_serial hash table
Also enable the test in the sanitizers workflow, as this was the last leak.

Fixes: 8a1596523a3d ("src/context: store obtained infos from system-info handler in context")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

.github/workflows/sanitizers.yml
@@ -35,7 +35,7 @@ jobs:
         build/test/bundle-test || true
         build/test/checksum-test
         build/test/config_file-test || true
-        build/test/context-test || true
+        build/test/context-test
         build/test/dm-test
         build/test/hash_index-test
         build/test/install-test || true

src/context.c
@@ -743,6 +743,8 @@ void r_context_clean(void)
 		g_clear_pointer(&context->mountprefix, g_free);
 		g_clear_pointer(&context->bootslot, g_free);
 		g_clear_pointer(&context->system_serial, g_free);
+		g_clear_pointer(&context->system_info, g_hash_table_destroy);
+
 		g_clear_pointer(&context->handlerextra, g_free);
 
 		g_clear_pointer(&context->install_info, r_context_install_info_free);