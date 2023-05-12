rauc/rauc

commit 86d6c12e388f70a0af9bf4145e1f7d9487c4b963

add casync `install-args` configuration key
When RAUC installs an update it might be useful to pass additional
argument to `casync extract`.

For example we could provide additional seeds or extra stores.

Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

src/config_file.c
@@ -461,6 +461,7 @@ gboolean load_config(const gchar *filename, RaucConfig **config, GError **error)
 	/* parse [casync] section */
 	c->store_path = key_file_consume_string(key_file, "casync", "storepath", NULL);
 	c->tmp_path = key_file_consume_string(key_file, "casync", "tmppath", NULL);
+	c->casync_install_args = key_file_consume_string(key_file, "casync", "install-args", NULL);
 	if (!check_remaining_keys(key_file, "casync", &ierror)) {
 		g_propagate_error(error, ierror);
 		res = FALSE;

test/config_file.c
@@ -68,6 +68,7 @@ path=/etc/rauc/keyring/\n\
 [casync]\n\
 storepath=/var/lib/default.castr/\n\
 tmppath=/tmp/\n\
+install-args=--verbose\n\
 \n\
 [slot.rescue.0]\n\
 description=Rescue partition\n\
@@ -120,6 +121,9 @@ install-same=false\n";
 	g_assert_cmpstr(config->statusfile_path, ==, "/mnt/persistent-rw-fs/system.raucs");
 	g_assert_cmpint(config->max_bundle_download_size, ==, 42);
 	g_assert_cmphex(config->bundle_formats_mask, ==, 0x2);
+	g_assert_cmpstr(config->store_path, ==, "/var/lib/default.castr/");
+	g_assert_cmpstr(config->tmp_path, ==, "/tmp/");
+	g_assert_cmpstr(config->casync_install_args, ==, "--verbose");
 
 	g_assert_nonnull(config->slots);
 	slotlist = g_hash_table_get_keys(config->slots);

include/config_file.h
@@ -50,6 +50,7 @@ typedef struct {
 	gchar *mount_prefix;
 	gchar *store_path;
 	gchar *tmp_path;
+	gchar *casync_install_args;
 	gboolean activate_installed;
 	gchar *statusfile_path;
 	gchar *keyring_path;

src/update_handler.c
@@ -232,6 +232,7 @@ static gboolean casync_extract(RaucImage *image, gchar *dest, int out_fd, const 
 {
 	g_autoptr(GSubprocessLauncher) launcher = NULL;
 	g_autoptr(GSubprocess) sproc = NULL;
+	g_auto(GStrv) casync_argvp = NULL;
 	GError *ierror = NULL;
 	gboolean res = FALSE;
 	g_autoptr(GPtrArray) args = g_ptr_array_new_full(5, g_free);
@@ -247,6 +248,21 @@ static gboolean casync_extract(RaucImage *image, gchar *dest, int out_fd, const 
 		g_ptr_array_add(args, g_strdup(store));
 	}
 	g_ptr_array_add(args, g_strdup("--seed-output=no"));
+	if (r_context()->config->casync_install_args != NULL) {
+		gboolean parse_res = FALSE;
+		parse_res = g_shell_parse_argv(r_context()->config->casync_install_args, NULL, &casync_argvp, &ierror);
+		if (!parse_res) {
+			res = parse_res;
+			g_propagate_prefixed_error(
+					error,
+					ierror,
+					"Failed to parse casync extra args: ");
+			goto out;
+		}
+		for (gchar **casync_args = casync_argvp; *casync_args != NULL; casync_args++) {
+			g_ptr_array_add(args, g_strdup(*casync_args));
+		}
+	}
 	g_ptr_array_add(args, g_strdup(image->filename));
 	g_ptr_array_add(args, g_strdup(out_fd >= 0 ? "-" : dest));
 	g_ptr_array_add(args, NULL);

commit 1426b5d70bba50ece6e5b22c129bfa4acb0d7af4

src/config_file: fix leak of casync_install_args string in RaucConfig
Fixes: 86d6c12e388f ("add casync `install-args` configuration key")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/config_file.c
@@ -859,6 +859,7 @@ void free_config(RaucConfig *config)
 	g_free(config->mount_prefix);
 	g_free(config->store_path);
 	g_free(config->tmp_path);
+	g_free(config->casync_install_args);
 	g_free(config->grubenv_path);
 	g_free(config->statusfile_path);
 	g_free(config->keyring_path);