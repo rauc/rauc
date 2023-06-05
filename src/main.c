rauc/rauc

commit 26026d6b71d06890382ac36c73c2a5162fe5e652

src/main: expose --cert and --key arguments for signing commands only
It is not necessary (but instead misleading) to have the --cert and
--key arguments as global options because they effectively apply to only
a very few commands.

Introduce a purpose-driven (rather than subcommand-driven) GOptionEntry
for this.

For backwards-compatibility of existing 'rauc {bundle, resign,
convert}' command lines calls, we keep the original global options, but
add the G_OPTION_FLAG_HIDDEN flag to not have them in the info output.

For the command-specific option group we must set G_OPTION_FLAG_NOALIAS
flag to turn of glib's automatic conflict resolution for arguments that
would prefix the arguments with 'groupname-'.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/main.c
@@ -35,6 +35,8 @@ gboolean info_dumpcert = FALSE;
 gboolean info_dumprecipients = FALSE;
 gboolean status_detailed = FALSE;
 gchar *output_format = NULL;
+gchar *keypath = NULL;
+gchar *certpath = NULL;
 gchar *signing_keyring = NULL;
 gchar *mksquashfs_args = NULL;
 gchar *casync_args = NULL;
@@ -2061,6 +2063,12 @@ static GOptionEntry entries_service[] = {
 	{0}
 };
 
+static GOptionEntry entries_signing[] = {
+	{"cert", '\0', G_OPTION_FLAG_NOALIAS, G_OPTION_ARG_FILENAME, &certpath, "signing cert file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
+	{"key", '\0', G_OPTION_FLAG_NOALIAS, G_OPTION_ARG_FILENAME, &keypath, "signing key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
+	{0}
+};
+
 static GOptionEntry entries_bundle_access[] = {
 	{"tls-cert", '\0', 0, G_OPTION_ARG_STRING, &access_args.tls_cert, "TLS client certificate file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
 	{"tls-key", '\0', 0, G_OPTION_ARG_STRING, &access_args.tls_key, "TLS client key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
@@ -2097,15 +2105,18 @@ static void create_option_groups(void)
 	if (ENABLE_CREATE) {
 		bundle_group  = g_option_group_new("bundle", "Bundle options:", "help dummy", NULL, NULL);
 		g_option_group_add_entries(bundle_group, entries_bundle);
+		g_option_group_add_entries(bundle_group, entries_signing);
 
 		resign_group  = g_option_group_new("resign", "Resign options:", "help dummy", NULL, NULL);
 		g_option_group_add_entries(resign_group, entries_resign);
+		g_option_group_add_entries(resign_group, entries_signing);
 
 		replace_group  = g_option_group_new("replace-signature", "Replace signature options:", "help dummy", NULL, NULL);
 		g_option_group_add_entries(replace_group, entries_replace);
 
 		convert_group = g_option_group_new("convert", "Convert options:", "help dummy", NULL, NULL);
 		g_option_group_add_entries(convert_group, entries_convert);
+		g_option_group_add_entries(convert_group, entries_signing);
 
 		encrypt_group = g_option_group_new("encrypt", "Encryption options:", "help dummy", NULL, NULL);
 		g_option_group_add_entries(encrypt_group, entries_encryption);
@@ -2132,13 +2143,14 @@ static void create_option_groups(void)
 static void cmdline_handler(int argc, char **argv)
 {
 	gboolean help = FALSE, debug = FALSE, version = FALSE;
-	gchar *confpath = NULL, *certpath = NULL, *keypath = NULL, *keyring = NULL, **intermediate = NULL, *mount = NULL;
+	gchar *confpath = NULL, *keyring = NULL, **intermediate = NULL, *mount = NULL;
 	char *cmdarg = NULL;
 	g_autoptr(GOptionContext) context = NULL;
 	GOptionEntry entries[] = {
 		{"conf", 'c', 0, G_OPTION_ARG_FILENAME, &confpath, "config file", "FILENAME"},
-		{"cert", '\0', 0, G_OPTION_ARG_FILENAME, &certpath, "cert file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
-		{"key", '\0', 0, G_OPTION_ARG_FILENAME, &keypath, "key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
+		/* NOTE: cert and key kept for backwards-compatibility, but made invisible */
+		{"cert", '\0', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_FILENAME, &certpath, "cert file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
+		{"key", '\0', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_FILENAME, &keypath, "key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
 		{"keyring", '\0', 0, G_OPTION_ARG_FILENAME, &keyring, "keyring file", "PEMFILE"},
 		{"intermediate", '\0', 0, G_OPTION_ARG_FILENAME_ARRAY, &intermediate, "intermediate CA file name", "PEMFILE"},
 		{"mount", '\0', 0, G_OPTION_ARG_FILENAME, &mount, "mount prefix", "PATH"},

test/rauc.t
@@ -321,6 +321,16 @@ test_expect_success "rauc info invalid" "
 
 test_expect_success "rauc bundle" "
   rm -f ${TEST_TMPDIR}/out.raucb &&
+  rauc bundle \
+    --cert $SHARNESS_TEST_DIRECTORY/openssl-ca/dev/autobuilder-1.cert.pem \
+    --key $SHARNESS_TEST_DIRECTORY/openssl-ca/dev/private/autobuilder-1.pem \
+    $SHARNESS_TEST_DIRECTORY/install-content ${TEST_TMPDIR}/out.raucb &&
+  rauc -c $SHARNESS_TEST_DIRECTORY/test.conf info ${TEST_TMPDIR}/out.raucb &&
+  test -f ${TEST_TMPDIR}/out.raucb
+"
+
+test_expect_success "rauc bundle (test compatibiltiy for cert/key args before subcommand)" "
+  rm -f ${TEST_TMPDIR}/out.raucb &&
   rauc \
     --cert $SHARNESS_TEST_DIRECTORY/openssl-ca/dev/autobuilder-1.cert.pem \
     --key $SHARNESS_TEST_DIRECTORY/openssl-ca/dev/private/autobuilder-1.pem \

commit eba0f9a820fc869c3d9cb2873d419dbe1c440320

src/main: add missing --key argument for info, extract, extract-signature
For the decryption use case, this specifies the decryption key.
The global --key options has been replaced by command-specific ones but
the purpose of '--key' for 'rauc info' and the others has not been
considered.

Since the former global options are still available but hidden, using
the options always worked but it has not been shown in help output or
man page.

Fixes: 26026d6b ("src/main: expose --cert and --key arguments for
signing commands only").

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

rauc.1
@@ -164,6 +164,10 @@ Extract the bundle content to a directory.
 .RS 4
 
 .TP
+\fB\-\-key=\fR\fIPEMFILE\fR|\fIPKCS11-URL\fR
+use given decryption key file or the decryption key referenced by the given PKCS#11 URL
+
+.TP
 \fB\-\-trust\-environment\fR
 trust environment and skip bundle access checks
 
@@ -178,6 +182,10 @@ Extract the bundle signature.
 \fBOptions:\fR
 
 .RS 4
+
+.TP
+\fB\-\-key=\fR\fIPEMFILE\fR|\fIPKCS11-URL\fR
+use given decryption key file or the decryption key referenced by the given PKCS#11 URL
 
 .TP
 \fB\-\-trust\-environment\fR
@@ -290,6 +298,10 @@ disable bundle verification
 .TP
 \fB\-\-no\-check\-time\fR
 don't check validity period of certificates against current time
+
+.TP
+\fB\-\-key=\fR\fIPEMFILE\fR|\fIPKCS11-URL\fR
+use given decryption key file or the decryption key referenced by the given PKCS#11 URL
 
 .TP
 \fB\-\-output\-format=\fR[\fBreadable\fR|\fBshell\fR|\fBjson\fR|\fBjson-pretty\fR|\fBjson-2\fR]

src/main.c
@@ -2139,11 +2139,13 @@ static GOptionEntry entries_convert[] = {
 };
 
 static GOptionEntry entries_extract_signature[] = {
+	{"key", '\0', G_OPTION_FLAG_NOALIAS, G_OPTION_ARG_FILENAME, &keypath, "decryption key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
 	{"trust-environment", '\0', 0, G_OPTION_ARG_NONE, &trust_environment, "trust environment and skip bundle access checks", NULL},
 	{0}
 };
 
 static GOptionEntry entries_extract[] = {
+	{"key", '\0', G_OPTION_FLAG_NOALIAS, G_OPTION_ARG_FILENAME, &keypath, "decryption key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
 	{"trust-environment", '\0', 0, G_OPTION_ARG_NONE, &trust_environment, "trust environment and skip bundle access checks", NULL},
 	{0}
 };
@@ -2151,6 +2153,7 @@ static GOptionEntry entries_extract[] = {
 static GOptionEntry entries_info[] = {
 	{"no-verify", '\0', 0, G_OPTION_ARG_NONE, &verification_disabled, "disable bundle verification", NULL},
 	{"no-check-time", '\0', 0, G_OPTION_ARG_NONE, &no_check_time, "don't check validity period of certificates against current time", NULL},
+	{"key", '\0', G_OPTION_FLAG_NOALIAS, G_OPTION_ARG_FILENAME, &keypath, "decryption key file or PKCS#11 URL", "PEMFILE|PKCS11-URL"},
 	{"output-format", '\0', 0, G_OPTION_ARG_STRING, &output_format, "output format", "FORMAT"},
 	{"dump-cert", '\0', 0, G_OPTION_ARG_NONE, &info_dumpcert, "dump certificate", NULL},
 	{"dump-recipients", '\0', 0, G_OPTION_ARG_NONE, &info_dumprecipients, "dump recipients", NULL},