rauc/rauc

commit dbbb009e0d8f631f5354ae7166cdb8f56341f45c

src/bundle: add function to completely read device
By reading the all of the devices' content we can make sure that the
bundle is not corrupt before trying to install it.

Signed-off-by: Christian Hitz <christian.hitz@bbv.ch>

src/bundle.c
@@ -2502,6 +2502,48 @@ out:
 	return res;
 }
 
+static gboolean read_complete_dm_device(gchar *dev, GError **error)
+{
+	int fd = -1;
+	g_autofree void* buf = NULL;
+	ssize_t r;
+	gboolean ret = TRUE;
+
+	g_return_val_if_fail(dev != NULL, FALSE);
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+	fd = g_open(dev, O_RDONLY | O_CLOEXEC, 0);
+	if (fd < 0) {
+		int err = errno;
+		g_set_error(error,
+				G_FILE_ERROR,
+				g_file_error_from_errno(err),
+				"Failed to open %s: %s", dev, g_strerror(err));
+		return FALSE;
+	}
+
+	buf = g_malloc0(65536);
+
+	for (goffset chunk = 0;; chunk++) {
+		r = pread(fd, buf, sizeof(buf), chunk*sizeof(buf));
+		if (r == 0)
+			break;
+		else if (r < 0)	{
+			int err = errno;
+			g_set_error(error,
+					G_FILE_ERROR,
+					g_file_error_from_errno(err),
+					"Check %s device failed between %"G_GOFFSET_FORMAT " and %"G_GOFFSET_FORMAT " bytes with error: %s", dev, chunk*sizeof(buf), (chunk+1)*sizeof(buf), g_strerror(err));
+			ret = FALSE;
+			break;
+		}
+	}
+
+	g_close(fd, NULL);
+
+	return ret;
+}
+
 /*
  * Sets up dm-verity for reading verity bundles.
  *

commit 43e1ff3018d302e4cdb6439390684a0ed03542d7

src/bundle: fix read size for read_complete_dm_device
The pread call confused the pointer size with the buffer size. Use a local
constant for the size to make it easier to read.

Fixes: dbbb009e0d8f ("src/bundle: add function to completely read device")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/bundle.c
@@ -2514,6 +2514,7 @@ out:
 static gboolean read_complete_dm_device(gchar *dev, GError **error)
 {
 	int fd = -1;
+	const goffset chunk_size = 65536;
 	g_autofree void* buf = NULL;
 	ssize_t r;
 	gboolean ret = TRUE;
@@ -2531,10 +2532,10 @@ static gboolean read_complete_dm_device(gchar *dev, GError **error)
 		return FALSE;
 	}
 
-	buf = g_malloc0(65536);
+	buf = g_malloc0(chunk_size);
 
 	for (goffset chunk = 0;; chunk++) {
-		r = pread(fd, buf, sizeof(buf), chunk*sizeof(buf));
+		r = pread(fd, buf, chunk_size, chunk*chunk_size);
 		if (r == 0)
 			break;
 		else if (r < 0)	{