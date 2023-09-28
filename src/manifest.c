rauc/rauc

commit 1e2d723f2e0b02d39b5cbe36e0b418fe591803ef

src/manifest: explicit error when using [handler] args without filename
Despite it was never documented or really intended, it was possible to use
the 'args' key in the manifest's '[handler]' section to pass arguments
to (system.conf-defined) handlers.
However, the actual intention of this key was to allow passing
additional arguments to the (manifest-defined) full custom handler.

Since we have just removed support for using the 'args' key for
system.conf-defined handlers, we now explicitly error-out in manifest
parsing in cases where 'args' was set but 'filename' (i.e. full custom
handler) was not.

This will prevent users from accidentally creating incompatible bundles.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/manifest.c
@@ -224,6 +224,10 @@ static gboolean parse_manifest(GKeyFile *key_file, RaucManifest **manifest, GErr
 	/* parse [handler] section */
 	raucm->handler_name = key_file_consume_string(key_file, "handler", "filename", NULL);
 	raucm->handler_args = key_file_consume_string(key_file, "handler", "args", NULL);
+	if (raucm->handler_args && !raucm->handler_name) {
+		g_set_error(error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_PARSE,
+				"Setting 'args' requires a full custom handler to be defined under 'filename' in group '[handler]'.");
+	}
 	if (!check_remaining_keys(key_file, "handler", &ierror)) {
 		g_propagate_error(error, ierror);
 		return FALSE;

commit 0662ea22b8c434a4cdf5ed2950c4e706ab08d17d

src/manifest: add missing return on error
Credit to OSS-Fuzz for finding this issue.

Fixes: 1e2d723f ("src/manifest: explicit error when using [handler] args without filename")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/manifest.c
@@ -250,6 +250,7 @@ static gboolean parse_manifest(GKeyFile *key_file, RaucManifest **manifest, GErr
 	if (raucm->handler_args && !raucm->handler_name) {
 		g_set_error(error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_PARSE,
 				"Setting 'args' requires a full custom handler to be defined under 'filename' in group '[handler]'.");
+		return FALSE;
 	}
 	if (!check_remaining_keys(key_file, "handler", &ierror)) {
 		g_propagate_error(error, ierror);