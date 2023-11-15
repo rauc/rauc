rauc/rauc

commit f594221bbec07f83cac1aded81c91f8c4fb7746e

utils: add fakeroot helpers
These support a persistent fakeroot environment, so multiple subprocesses can
be executed within the same environment (for example extracting a tar and
generating a squashfs).

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/utils.c
@@ -651,3 +651,65 @@ gboolean r_syncfs(const gchar *path, GError **error)
 
 	return TRUE;
 }
+
+gchar* r_fakeroot_init(GError **error)
+{
+	GError *ierror = NULL;
+	g_autofree gchar *tmpdir = NULL;
+	g_autofree gchar *tmpfile = NULL;
+	int fd = -1;
+
+	g_return_val_if_fail(error == NULL || *error == NULL, NULL);
+
+	tmpdir = g_dir_make_tmp("rauc-fakeroot-XXXXXX", &ierror);
+	if (tmpdir == NULL) {
+		g_propagate_prefixed_error(error, ierror, "Failed to create tmp dir: ");
+		return NULL;
+	}
+
+	tmpfile = g_build_filename(tmpdir, "environment", NULL);
+	fd = g_open(tmpfile, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1) {
+		int err = errno;
+		g_set_error(error,
+				G_FILE_ERROR,
+				g_file_error_from_errno(err),
+				"Failed to create %s: %s", tmpfile, g_strerror(err));
+		return FALSE;
+	}
+
+	return g_steal_pointer(&tmpfile);
+}
+
+void r_fakeroot_add_args(GPtrArray *args, const gchar *envpath)
+{
+	g_return_if_fail(args != NULL);
+
+	if (!envpath)
+		return;
+
+	g_assert(g_path_is_absolute(envpath));
+	g_assert(g_str_has_suffix(envpath, "/environment"));
+
+	g_ptr_array_add(args, g_strdup("fakeroot"));
+	g_ptr_array_add(args, g_strdup("-s"));
+	g_ptr_array_add(args, g_strdup(envpath));
+	g_ptr_array_add(args, g_strdup("-i"));
+	g_ptr_array_add(args, g_strdup(envpath));
+	g_ptr_array_add(args, g_strdup("--"));
+}
+
+gboolean r_fakeroot_cleanup(const gchar *envpath, GError **error)
+{
+	g_autofree gchar *tmpdir = g_path_get_dirname(envpath);
+
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+	if (!envpath)
+		return TRUE;
+
+	g_assert(g_path_is_absolute(envpath));
+	g_assert(g_str_has_suffix(envpath, "/environment"));
+
+	return rm_tree(tmpdir, error);
+}

test/utils.c
@@ -165,6 +165,97 @@ static void update_symlink_test(void)
 	g_clear_pointer(&target, g_free);
 }
 
+static void fakeroot_test(void)
+{
+	g_autofree GError *ierror = NULL;
+	g_autofree gchar *envpath = NULL;
+	g_autoptr(GPtrArray) chown_args = g_ptr_array_new_with_free_func(g_free);
+	g_autoptr(GPtrArray) test_args = g_ptr_array_new_with_free_func(g_free);
+	g_autoptr(GSubprocess) sproc = NULL;
+	g_autofree gchar *stdout = NULL;
+	gboolean res = FALSE;
+
+	if (g_strcmp0(g_get_host_name(), "qemu-test") !=0) {
+		g_test_message("fakeroot test is only supported under qemu-test");
+		g_test_skip("not running under qemu-test");
+		return;
+	}
+
+	envpath = r_fakeroot_init(&ierror);
+	g_assert_no_error(ierror);
+	g_assert_nonnull(envpath);
+
+	/* fakeroot has not been used yet, but the env should be created by _init */
+	g_assert_true(g_file_test(envpath, G_FILE_TEST_EXISTS));
+
+	/* check that /etc/hostname is owned by root */
+	r_fakeroot_add_args(test_args, envpath);
+	g_assert_cmpuint(test_args->len, ==, 6);
+
+	g_ptr_array_add(test_args, g_strdup("stat"));
+	g_ptr_array_add(test_args, g_strdup("-c"));
+	g_ptr_array_add(test_args, g_strdup("%u"));
+	g_ptr_array_add(test_args, g_strdup("/etc/hostname"));
+	g_ptr_array_add(test_args, NULL);
+
+	sproc = r_subprocess_newv(test_args, G_SUBPROCESS_FLAGS_STDOUT_PIPE, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_nonnull(sproc);
+
+	res = g_subprocess_communicate_utf8(sproc, NULL, NULL, &stdout, NULL, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_true(res);
+	g_assert_cmpstr(g_strchomp(stdout), ==, "0");
+
+	res = g_subprocess_wait_check(sproc, NULL, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_true(res);
+
+	g_clear_pointer(&stdout, g_free);
+	g_clear_pointer(&sproc, g_object_unref);
+
+	/* chown /etc/hostname to user 1*/
+	r_fakeroot_add_args(chown_args, envpath);
+
+	g_ptr_array_add(chown_args, g_strdup("chown"));
+	g_ptr_array_add(chown_args, g_strdup("1"));
+	g_ptr_array_add(chown_args, g_strdup("/etc/hostname"));
+	g_ptr_array_add(chown_args, NULL);
+
+	sproc = r_subprocess_newv(chown_args, G_SUBPROCESS_FLAGS_NONE, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_nonnull(sproc);
+
+	res = g_subprocess_wait_check(sproc, NULL, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_true(res);
+
+	g_clear_pointer(&sproc, g_object_unref);
+
+	/* fakeroot has been used yet, the env should exist */
+	g_assert_true(g_file_test(envpath, G_FILE_TEST_EXISTS));
+
+	/* check that /etc/hostname is not owned by root */
+	sproc = r_subprocess_newv(test_args, G_SUBPROCESS_FLAGS_STDOUT_PIPE, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_nonnull(sproc);
+
+	res = g_subprocess_communicate_utf8(sproc, NULL, NULL, &stdout, NULL, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_true(res);
+	g_assert_cmpstr(g_strchomp(stdout), ==, "1");
+
+	res = g_subprocess_wait_check(sproc, NULL, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_true(res);
+
+	g_clear_pointer(&sproc, g_object_unref);
+
+	res = r_fakeroot_cleanup(envpath, &ierror);
+	g_assert_no_error(ierror);
+	g_assert_true(res);
+}
+
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "C");
@@ -177,6 +268,7 @@ int main(int argc, char *argv[])
 	g_test_add_func("/utils/get_sectorsize", get_sectorsize_test);
 	g_test_add_func("/utils/get_device_size", get_device_size_test);
 	g_test_add_func("/utils/update_symlink", update_symlink_test);
+	g_test_add_func("/utils/fakeroot", fakeroot_test);
 
 	return g_test_run();
 }

include/utils.h
@@ -294,3 +294,37 @@ G_GNUC_WARN_UNUSED_RESULT;
  */
 gboolean r_syncfs(const gchar *path, GError **error)
 G_GNUC_WARN_UNUSED_RESULT;
+
+/**
+ * Create a temporary directory for the fakeroot environment file.
+ *
+ * @param error return location for a GError, or NULL
+ *
+ * @return path to the env file, NULL on error
+ */
+gchar* r_fakeroot_init(GError **error)
+G_GNUC_WARN_UNUSED_RESULT;
+
+/**
+ * Add fakeroot startup arguments to the array.
+ *
+ * The env file needs to be created with r_fakeroot_init first.
+ * Does nothing if the path is NULL.
+ *
+ * @param args the GPtrArray to modify
+ * @param envpath path to the env file
+ */
+void r_fakeroot_add_args(GPtrArray *args, const gchar *envpath);
+
+/**
+ * Removes the temporary directory containing the fakeroot environment file.
+ *
+ * Does nothing if the path is NULL.
+ *
+ * @param envpath path to the env file
+ * @param error return location for a GError, or NULL
+ *
+ * @return TRUE if the cleanup was successful, FALSE otherwise
+ */
+gboolean r_fakeroot_cleanup(const gchar *envpath, GError **error)
+G_GNUC_WARN_UNUSED_RESULT;

commit 3111473ba29cb99fcc4d6dd5519002b5c5c21353

src/utils: fix file descriptor leak
Fixes: f594221b ("utils: add fakeroot helpers")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/utils.c
@@ -657,7 +657,7 @@ gchar* r_fakeroot_init(GError **error)
 	GError *ierror = NULL;
 	g_autofree gchar *tmpdir = NULL;
 	g_autofree gchar *tmpfile = NULL;
-	int fd = -1;
+	g_auto(filedesc) fd = -1;
 
 	g_return_val_if_fail(error == NULL || *error == NULL, NULL);
 