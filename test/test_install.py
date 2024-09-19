16575b41c0489fe0e3474c3f3f5572f0def87589
Enrico Joerns <ejo@pengutronix.de>
2024-09-19 16:26:42 +0200

    test/test_install: test installing twice with 'external'
    With the changes made for timestamp-based slot selection, it is possible
    to install to both inactive slot groups by calling 'rauc install' twice.
    
    This extends the existing test case 'test_install_rauc_external' to
    cover that, too.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- test/test_install.py
+++ test/test_install.py
@@ -151,6 +151,13 @@ def test_install_rauc_external(rauc_dbus_service_with_system_external, tmp_path)
 
     assert exitcode == 0
     assert os.path.getsize(tmp_path / "images/rootfs-1") > 0
+    assert os.path.getsize(tmp_path / "images/rootfs-0") == 0
+
+    out, err, exitcode = run(f"rauc install {tmp_path}/good-bundle.raucb")
+
+    assert exitcode == 0
+    assert os.path.getsize(tmp_path / "images/rootfs-1") > 0
+    assert os.path.getsize(tmp_path / "images/rootfs-0") > 0
 
 
 @no_service
