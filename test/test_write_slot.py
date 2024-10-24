373269b6191536f730a3a1bcb3de2c7829771539
Enrico Joerns <ejo@pengutronix.de>
2024-10-24 07:11:09 +0200

    test/test_write_slot: test actually writing something
    So far test did only check the error cases, not the 'good case' of
    actually writing a slot.
    
    While at it, also test unsupported image type handling.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- test/test_write_slot.py
+++ test/test_write_slot.py
@@ -25,3 +25,17 @@ def test_write_slot_readonly():
     out, err, exitcode = run("rauc -c test.conf write-slot rescue.0 install-content/appfs.img")
     assert exitcode == 1
     assert "Reject writing to readonly slot" in err
+
+
+def test_write_slot(rauc_no_service):
+    out, err, exitcode = run(f"{rauc_no_service} write-slot rootfs.0 install-content/appfs.img")
+    assert exitcode == 0
+    assert "Slot written successfully" in out
+
+
+def test_write_slot_no_handler(tmp_path, rauc_no_service):
+    open(tmp_path / "image.xyz", mode="w").close()
+
+    out, err, exitcode = run(f"{rauc_no_service} write-slot rootfs.0 {tmp_path}/image.xyz")
+    assert exitcode == 1
+    assert f"Unsupported image {tmp_path}/image.xyz for slot type ext4" in err
