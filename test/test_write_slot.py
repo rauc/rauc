5e0e534c9d19e9946767ca106e314bdc0e7445fa
Enrico Joerns <ejo@pengutronix.de>
2024-11-23 22:51:51 +0100

    test/test_write_slot: write-slot must use slot name
    The 'write-slot' subcommand expects the slot name as parameter. Despite
    the tests fail anyawy, we should use proper arguments here to avoid
    confusion. Replace bootnames by slot names thus.
    
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- test/test_write_slot.py
+++ test/test_write_slot.py
@@ -6,21 +6,21 @@ from conftest import needs_emmc
 
 
 def test_write_slot_invalid_local_paths():
-    out, err, exitcode = run("rauc -c test.conf write-slot system0 foo")
+    out, err, exitcode = run("rauc -c test.conf write-slot rootfs.0 foo")
     assert exitcode == 1
     assert "No such file or directory" in err
 
-    out, err, exitcode = run("rauc -c test.conf write-slot system0 foo.raucb")
+    out, err, exitcode = run("rauc -c test.conf write-slot rootfs.0 foo.raucb")
     assert exitcode == 1
     assert "No such file or directory" in err
 
-    out, err, exitcode = run("rauc -c test.conf write-slot system0 /path/to/foo.raucb")
+    out, err, exitcode = run("rauc -c test.conf write-slot rootfs.0 /path/to/foo.raucb")
     assert exitcode == 1
     assert "No such file or directory" in err
 
 
 def test_write_slot_invalid_slot():
-    out, err, exitcode = run("rauc -c test.conf write-slot systemx install-content/rootfs.img")
+    out, err, exitcode = run("rauc -c test.conf write-slot dummy install-content/rootfs.img")
     assert exitcode == 1
     assert "No matching slot found for given slot name" in err
 
