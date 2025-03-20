447476981c891d13f21055831f5273c150c9c93b
Gaël PORTAY <gael.portay@rtone.fr>
2025-03-20 14:03:09 +0100

    src/slot: remove unnecessary goto
    This returns the slot direclty and removes the unnecessary goto.

--- src/slot.c
+++ src/slot.c
@@ -67,14 +67,11 @@ RaucSlot *r_slot_find_by_device(GHashTable *slots, const gchar *device)
 	g_hash_table_iter_init(&iter, slots);
 	while (g_hash_table_iter_next(&iter, NULL, (gpointer*) &slot)) {
 		if (g_strcmp0(slot->device, device) == 0) {
-			goto out;
+			return slot;
 		}
 	}
 
-	slot = NULL;
-
-out:
-	return slot;
+	return NULL;
 }
 
 RaucSlot *r_slot_get_booted(GHashTable *slots)
@@ -87,14 +84,11 @@ RaucSlot *r_slot_get_booted(GHashTable *slots)
 	g_hash_table_iter_init(&iter, slots);
 	while (g_hash_table_iter_next(&iter, NULL, (gpointer*) &slot)) {
 		if (slot->state == ST_BOOTED) {
-			goto out;
+			return slot;
 		}
 	}
 
-	slot = NULL;
-
-out:
-	return slot;
+	return NULL;
 }
 
 /* returns string representation of slot state */
