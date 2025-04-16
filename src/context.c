rauc/rauc

commit 295fa3a5059784ef65651dd1f31e6d88ae64a2f8

src/context.c: fully free context in r_context_clean()
r_context_clean() freed all but the context itself. The context setup
however, checks for context to be initialized (non-NULL) to trigger
context initialization.

Thus to fully re-setup the context after r_context_clean(), it should
also free it.

Fix test/signature.test that cleaned context in teardown method but did
not set it up in set_up method.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/context.c
@@ -655,5 +655,7 @@ void r_context_clean(void)
 		if (context->config) {
 			context->config->keyring_path = NULL;
 		}
+
+		g_clear_pointer(&context, g_free);
 	}
 }

test/signature.c
@@ -21,6 +21,8 @@ typedef struct {
 static void signature_set_up(SignatureFixture *fixture,
 		gconstpointer user_data)
 {
+	r_context_conf();
+
 	fixture->content = read_file("test/openssl-ca/manifest", NULL);
 	g_assert_nonnull(fixture->content);
 	fixture->sig = NULL;
@@ -738,7 +740,6 @@ static void signature_cmsverify_pathdir_path(SignatureFixture *fixture,
 int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "C");
-	r_context_conf();
 
 	g_assert(test_prepare_dummy_file("test/", "random.dat",
 			256 * 1024, "/dev/urandom") == 0);

commit 706ccc68b3d1e3bfe70693a70a76965884b19817

src/context: fix library reinitialization
In the test suite, we sometimes recreate the context, which caused
multiple initialization of the network and signature dependencies. With
OpenSSL 3.5, this leads to purpose ID collisions, as we try to add the
same purpose multiple times.

Avoid this by remembering if we have initialized our dependencies
already.

Fixes: 295fa3a50597 ("src/context.c: fully free context in r_context_clean()")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

src/context.c
@@ -780,7 +780,9 @@ void r_context_register_progress_callback(progress_callback progress_cb)
 
 RaucContext *r_context_conf(void)
 {
-	if (context == NULL) {
+	static gboolean initialized = FALSE;
+
+	if (!initialized) {
 		GError *ierror = NULL;
 
 		// let us handle broken pipes explicitly
@@ -797,6 +799,10 @@ RaucContext *r_context_conf(void)
 			return NULL;
 		}
 
+		initialized = TRUE;
+	}
+
+	if (context == NULL) {
 		context = g_new0(RaucContext, 1);
 		context->progress = NULL;
 		context->install_info = g_new0(RContextInstallationInfo, 1);