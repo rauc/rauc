rauc/rauc

commit fa87117338b15c75d8c03942ce3766eccbe2a9b8

src/update_handler: update boot partition reporting
Since r_emmc_read_bootpart() now only returns either a valid boot
partition number or 'no boot enabled', we can simplify the printout.

We allow the 'no boot enabled' configuration here, but emit a warning to
inform the user.

Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

src/update_handler.c
@@ -2208,21 +2208,17 @@ static gboolean img_to_boot_emmc_handler(RaucImage *image, RaucSlot *dest_slot, 
 		g_propagate_error(error, ierror);
 		goto out;
 	}
-	switch (part_active) {
-		case -1:
-			part_active_str = g_strdup("<none>");
-			break;
-		case 6:
-			part_active_str = g_strdup(realdev);
-			break;
-		default:
-			part_active_str = g_strdup_printf("%sboot%d", realdev, part_active);
+
+	if (part_active == -1) {
+		g_warning("eMMC device was not enabled for booting, yet. Ignoring.");
+		/* For simplicity: Consider boot1 to be active */
+		part_active = 1;
+	} else {
+		g_message("Found active eMMC boot partition %sboot%d", realdev, part_active);
 	}
-	g_message("Found active eMMC boot partition %s", part_active_str);
 
 	/* create a temporary RaucSlot with the actual (currently inactive) boot
-	 * partition as device; for simplicity reasons: in case the user partition
-	 * is active use mmcblkXboot1, in case no partition is active use mmcblkXboot0
+	 * partition as device.
 	 */
 	part_slot = g_new0(RaucSlot, 1);
 	part_slot->device = g_strdup_printf(

test/test_write_slot.py
@@ -61,7 +61,7 @@ def test_write_boot_emmc(system):
     out, err, exitcode = run(f"{system.prefix} write-slot bootloader.0 install-content/rootfs.img")
     assert exitcode == 0
     assert "Slot written successfully" in out
-    assert "Found active eMMC boot partition <none>" in err
+    assert "eMMC device was not enabled for booting, yet. Ignoring." in err
     assert f"Boot partition {device}boot0 is now active" in err
 
     out, err, exitcode = run(f"{system.prefix} write-slot bootloader.0 install-content/rootfs.img")

commit 41678221e203276bb4334b418505c092b270ce80

src/update_handler: fix emmc bootpart printout
And remove the unused part_active_str variable.

Fixes: fa871173 ("src/update_handler: update boot partition reporting")
Signed-off-by: Enrico Jörns <ejo@pengutronix.de>

src/update_handler.c
@@ -2206,7 +2206,6 @@ static gboolean img_to_boot_emmc_handler(RaucImage *image, RaucSlot *dest_slot, 
 	gboolean res = FALSE;
 	int out_fd;
 	gint part_active;
-	g_autofree gchar *part_active_str = NULL;
 	g_autofree gchar *realdev = NULL;
 	gint part_active_after;
 	g_autoptr(GUnixOutputStream) outstream = NULL;
@@ -2345,7 +2344,7 @@ static gboolean img_to_boot_emmc_handler(RaucImage *image, RaucSlot *dest_slot, 
 	 * for simplicity reasons: in case the user partition is active use
 	 * mmcblkXboot1, in case no partition is active use mmcblkXboot0
 	 */
-	g_debug("Toggling active eMMC boot partition %s -> %s", part_active_str,
+	g_debug("Toggling active eMMC boot partition %sboot%d -> %s", realdev, part_active,
 			part_slot->device);
 	res = r_emmc_write_bootpart(
 			part_slot->device,