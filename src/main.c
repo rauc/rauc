3139f105140270a5521e25db83307d09c06ef558
Enrico Jörns <ejo@pengutronix.de>
2025-11-11 23:55:15 +0100

    src/main: warn on using --keyring argument for 'install'
    If the service is disabled, we should not allow passing --keyring to
    'install', since this does not have any effect.
    
    Signed-off-by: Enrico Jörns <ejo@pengutronix.de>

--- src/main.c
+++ src/main.c
@@ -254,6 +254,14 @@ static gboolean install_start(int argc, char **argv)
 		goto out;
 	}
 
+	if (ENABLE_SERVICE) {
+		if (keyring) {
+			g_printerr("Using the --keyring argument for 'rauc install' is not supported!\n");
+			r_exit_status = 1;
+			goto out;
+		}
+	}
+
 	bundlelocation = resolve_bundle_path(argv[2]);
 	if (bundlelocation == NULL)
 		goto out;
