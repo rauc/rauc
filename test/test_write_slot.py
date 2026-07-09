rauc/rauc

commit 5c88aa9470be755dfe7e30f3c9fca928cae9c7f4

src/main: add '--image-type' option to write-slot command
Signed-off-by: Enrico Jörns <ejo@pengutronix.de>

src/main.c
@@ -54,6 +54,7 @@ gchar *handler_args = NULL;
 gchar *bootslot = NULL;
 gchar *installation_txn = NULL;
 gchar *require_manifest_hash = NULL;
+gchar *write_slot_image_type = NULL;
 gboolean utf8_supported = FALSE;
 RaucBundleAccessArgs access_args = {0};
 
@@ -498,13 +499,23 @@ static gboolean write_slot_start(int argc, char **argv)
 	image->checksum.size = g_file_info_get_size(info);
 	image->filename = g_strdup(argv[3]);
 
-	const gchar *derived_type = derive_image_type_from_filename_pattern(image->filename);
-	if (derived_type == NULL) {
-		g_printerr("Unable to map extension of file '%s' to known image type\n", image->filename);
+	if (write_slot_image_type) {
+		image->type = g_strdup(write_slot_image_type);
+	} else {
+		const gchar *derived_type = derive_image_type_from_filename_pattern(image->filename);
+		if (derived_type == NULL) {
+			g_printerr("Unable to map extension of file '%s' to known image type\n", image->filename);
+			r_exit_status = 1;
+			return TRUE;
+		}
+		image->type = g_strdup(derived_type);
+	}
+
+	if (!is_image_type_supported(image->type)) {
+		g_printerr("Unsupported image type '%s'\n", write_slot_image_type);
 		r_exit_status = 1;
 		return TRUE;
 	}
-	image->type = g_strdup(derived_type);
 
 	/* retrieve RaucSlot */
 	slot = g_hash_table_lookup(r_context()->config->slots, argv[2]);
@@ -2615,6 +2626,11 @@ static GOptionEntry entries_status[] = {
 	{0}
 };
 
+static GOptionEntry entries_write_slot[] = {
+	{"image-type", 'l', 0, G_OPTION_ARG_STRING, &write_slot_image_type, "Select explicit image type to use.", NULL},
+	{0}
+};
+
 static GOptionEntry entries_service[] = {
 	{"handler-args", '\0', 0, G_OPTION_ARG_STRING, &handler_args, "extra arguments for full custom handler", "ARGS"},
 	{"override-boot-slot", '\0', 0, G_OPTION_ARG_STRING, &bootslot, "override auto-detection of booted slot", "BOOTNAME"},
@@ -2652,6 +2668,7 @@ static GOptionGroup *extract_signature_group;
 static GOptionGroup *extract_group;
 static GOptionGroup *info_group;
 static GOptionGroup *status_group;
+static GOptionGroup *write_slot_group;
 static GOptionGroup *service_group;
 
 static void create_option_groups(void)
@@ -2694,6 +2711,9 @@ static void create_option_groups(void)
 
 	status_group = g_option_group_new("status", "Status options:", "help dummy", NULL, NULL);
 	g_option_group_add_entries(status_group, entries_status);
+
+	write_slot_group = g_option_group_new("write-slot", "Write-Slot options:", "help dummy", NULL, NULL);
+	g_option_group_add_entries(write_slot_group, entries_write_slot);
 
 	service_group = g_option_group_new("service", "Service options:", "help dummy", NULL, NULL);
 	g_option_group_add_entries(service_group, entries_service);
@@ -2793,7 +2813,7 @@ static void cmdline_handler(int argc, char **argv)
 		{WRITE_SLOT, "write-slot", "write-slot <SLOTNAME> <IMAGE>",
 		 "Manually write image to slot (using slot update handler).\n"
 		 "This bypasses all other update logic and is for development or special use only!",
-		 write_slot_start, NULL, R_CONTEXT_CONFIG_MODE_REQUIRED, FALSE},
+		 write_slot_start, write_slot_group, R_CONTEXT_CONFIG_MODE_REQUIRED, FALSE},
 #if ENABLE_SERVICE == 1
 		{SERVICE, "service", "service",
 		 "Start RAUC service",

test/test_write_slot.py
@@ -37,6 +37,23 @@ def test_write_slot(rauc_no_service):
     assert "Slot written successfully" in out
 
 
+def test_write_slot_image_type(tmp_path, rauc_no_service):
+    """
+    Tests writing image with non-standard '.bin' extension to an 'ext4' slot:
+    - First run without explicit '--image-type' option set (should fail).
+    - Second run *with* this option set (should succeed).
+    """
+    open(tmp_path / "image.bin", mode="w").close()
+
+    out, err, exitcode = run(f"{rauc_no_service} write-slot rootfs.0 {tmp_path}/image.bin")
+    assert exitcode == 1
+    assert f"Unable to map extension of file '{tmp_path}/image.bin' to known image type" in err
+
+    out, err, exitcode = run(f"{rauc_no_service} write-slot --image-type=raw rootfs.0 install-content/appfs.img")
+    assert exitcode == 0
+    assert "Slot written successfully" in out
+
+
 def test_write_slot_no_handler(tmp_path, rauc_no_service):
     open(tmp_path / "image.vfat", mode="w").close()
 

commit 3c2839d19fef93cbc7de4f6d0bb546a3fcbf9cbc

src/main: fix shortname option for write-slot --image-type to be -t
Accidentally, the shortname option was named '-l' instead of '-t'.

Rename it and provide a compatibility option and test both.

Fixes: 5c88aa94 ("src/main: add '--image-type' option to write-slot command")
Signed-off-by: Enrico Jörns <ejo@pengutronix.de>

src/main.c
@@ -2666,7 +2666,9 @@ static GOptionEntry entries_status[] = {
 };
 
 static GOptionEntry entries_write_slot[] = {
-	{"image-type", 'l', 0, G_OPTION_ARG_STRING, &write_slot_image_type, "Select explicit image type to use.", NULL},
+	{"image-type", 't', 0, G_OPTION_ARG_STRING, &write_slot_image_type, "Select explicit image type to use.", NULL},
+	/* The 'image-type' shortname was accidentally named 'l' before. This hidden option ensures compatibility with the old shortname */
+	{"image-type-compat", 'l', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_STRING, &write_slot_image_type, "Select explicit image type to use (compat).", NULL},
 	{0}
 };
 

test/test_write_slot.py
@@ -53,6 +53,14 @@ def test_write_slot_image_type(tmp_path, rauc_no_service):
     assert exitcode == 0
     assert "Slot written successfully" in out
 
+    out, err, exitcode = run(f"{rauc_no_service} write-slot -t raw rootfs.0 install-content/appfs.img")
+    assert exitcode == 0
+    assert "Slot written successfully" in out
+
+    out, err, exitcode = run(f"{rauc_no_service} write-slot -l raw rootfs.0 install-content/appfs.img")
+    assert exitcode == 0
+    assert "Slot written successfully" in out
+
 
 def test_write_slot_no_handler(tmp_path, rauc_no_service):
     open(tmp_path / "image.vfat", mode="w").close()