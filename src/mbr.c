68aa8c589e2437a178eb596f05df8bee89e73f4a
Jan Luebbe <jlu@pengutronix.de>
2026-07-29 11:50:44 +0200

    src/mbr: declare variables at the first assignment
    No functional change is intended.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- src/mbr.c
+++ src/mbr.c
@@ -46,11 +46,10 @@ G_STATIC_ASSERT(sizeof(struct mbr) == 512);
 
 static void get_hd_geometry(gint fd, guint8 *heads, guint8 *sectors)
 {
-	struct hd_geometry geometry = {};
-
 	g_return_if_fail(heads);
 	g_return_if_fail(sectors);
 
+	struct hd_geometry geometry = {};
 	if (ioctl(fd, HDIO_GETGEO, &geometry) == 0) {
 		*heads = geometry.heads;
 		*sectors = geometry.sectors;
@@ -65,7 +64,6 @@ static void get_hd_geometry(gint fd, guint8 *heads, guint8 *sectors)
 static gboolean validate_region(gint fd, guint64 start, guint64 size,
 		guint sector_size, GError **error)
 {
-	goffset device_size;
 	GError *ierror = NULL;
 
 	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
@@ -90,7 +88,7 @@ static gboolean validate_region(gint fd, guint64 start, guint64 size,
 		return FALSE;
 	}
 
-	device_size = get_device_size(fd, &ierror);
+	goffset device_size = get_device_size(fd, &ierror);
 	if (device_size == 0) {
 		g_propagate_error(error, ierror);
 		return FALSE;
@@ -130,12 +128,10 @@ static gboolean is_region_free(guint64 region_start, guint64 region_size,
 		const struct mbr_tbl_entry *partition_tbl, guint sector_size,
 		GError **error)
 {
-	guint64 p_start, p_end;
-	guint i;
-
 	g_return_val_if_fail(partition_tbl, FALSE);
 	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
 
+	guint i;
 	for (i = 0; i < MBR_NUMBER_OF_PARTITIONS; i++) {
 		if (i == BOOT_PARTITION_ENTRY)
 			continue;
@@ -144,9 +140,9 @@ static gboolean is_region_free(guint64 region_start, guint64 region_size,
 		if (partition_tbl[i].partition_size_le == 0)
 			continue;
 
-		p_start = (guint64)GUINT32_FROM_LE(partition_tbl[i].partition_start_le) * sector_size;
-		p_end = (guint64)GUINT32_FROM_LE(partition_tbl[i].partition_size_le) * sector_size +
-		        p_start - 1;
+		guint64 p_start = (guint64)GUINT32_FROM_LE(partition_tbl[i].partition_start_le) * sector_size;
+		guint64 p_end = (guint64)GUINT32_FROM_LE(partition_tbl[i].partition_size_le) * sector_size +
+		                p_start - 1;
 
 		if (region_start >= p_start && region_start <= p_end) {
 			g_set_error(error, R_UPDATE_ERROR, R_UPDATE_ERROR_FAILED,
@@ -205,16 +201,11 @@ static gboolean get_raw_partition_entry(gint fd,
 		struct mbr_tbl_entry *raw_entry,
 		const struct boot_switch_partition *partition, GError **error)
 {
-	guint32 start, size;
-	guint sector_size;
-	guint8 heads, sectors;
-
 	g_return_val_if_fail(raw_entry, FALSE);
 	g_return_val_if_fail(partition, FALSE);
 	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
 
-	sector_size = get_sectorsize(fd);
-
+	guint sector_size = get_sectorsize(fd);
 	if (partition->start % sector_size || partition->size % sector_size) {
 		g_set_error(error, R_UPDATE_ERROR, R_UPDATE_ERROR_FAILED,
 				"Partition start address or size is not a multiple"
@@ -222,12 +213,13 @@ static gboolean get_raw_partition_entry(gint fd,
 		return FALSE;
 	}
 
-	start = partition->start / sector_size;
-	size = partition->size / sector_size;
+	guint32 start = partition->start / sector_size;
+	guint32 size = partition->size / sector_size;
 
 	raw_entry->partition_start_le = GUINT32_TO_LE(start);
 	raw_entry->partition_size_le = GUINT32_TO_LE(size);
 
+	guint8 heads, sectors;
 	get_hd_geometry(fd, &heads, &sectors);
 
 	get_chs(&raw_entry->chs_start, start, heads, sectors);
@@ -242,10 +234,7 @@ gboolean r_mbr_switch_get_inactive_partition(const gchar *device,
 		guint64 region_start, guint64 region_size,
 		GError **error)
 {
-	struct mbr mbr = {};
 	GError *ierror = NULL;
-	struct mbr_tbl_entry *boot_part;
-	guint sector_size;
 
 	g_return_val_if_fail(device, FALSE);
 	g_return_val_if_fail(partition, FALSE);
@@ -253,13 +242,13 @@ gboolean r_mbr_switch_get_inactive_partition(const gchar *device,
 
 	g_auto(filedesc) fd = g_open(device, O_RDONLY);
 
-	sector_size = get_sectorsize(fd);
-
+	guint sector_size = get_sectorsize(fd);
 	if (!validate_region(fd, region_start, region_size, sector_size, &ierror)) {
 		g_propagate_error(error, ierror);
 		return FALSE;
 	}
 
+	struct mbr mbr = {};
 	if (!read_mbr(fd, &mbr, &ierror)) {
 		g_propagate_prefixed_error(error, ierror,
 				"Failed to read MBR:");
@@ -273,8 +262,7 @@ gboolean r_mbr_switch_get_inactive_partition(const gchar *device,
 		return FALSE;
 	}
 
-	boot_part = &mbr.partition_table[BOOT_PARTITION_ENTRY];
-
+	struct mbr_tbl_entry *boot_part = &mbr.partition_table[BOOT_PARTITION_ENTRY];
 	if (GUINT32_FROM_LE(boot_part->partition_start_le) == 0) {
 		g_set_error(error, R_UPDATE_ERROR, R_UPDATE_ERROR_FAILED,
 				"No boot partition found in entry %d",
@@ -303,8 +291,6 @@ gboolean r_mbr_switch_set_boot_partition(const gchar *device,
 		const struct boot_switch_partition *partition,
 		GError **error)
 {
-	struct mbr mbr = {};
-	struct mbr_tbl_entry *boot_part;
 	GError *ierror = NULL;
 
 	g_return_val_if_fail(device, FALSE);
@@ -319,14 +305,14 @@ gboolean r_mbr_switch_set_boot_partition(const gchar *device,
 		return FALSE;
 	}
 
+	struct mbr mbr = {};
 	if (!read_mbr(fd, &mbr, &ierror)) {
 		g_propagate_prefixed_error(error, ierror,
 				"Failed to read MBR:");
 		return FALSE;
 	}
 
-	boot_part = &mbr.partition_table[BOOT_PARTITION_ENTRY];
-
+	struct mbr_tbl_entry *boot_part = &mbr.partition_table[BOOT_PARTITION_ENTRY];
 	if (!get_raw_partition_entry(fd, boot_part, partition, &ierror)) {
 		g_propagate_prefixed_error(error, ierror,
 				"Failed to create new partition entry:");
